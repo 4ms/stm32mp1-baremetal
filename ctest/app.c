5055ea715639b5ad01e780abb88a5e54917dca2c
Dan Green <danngreen1@gmail.com>
2020-12-10 14:47:20 -0800

    C evironment app (WIP)

--- ctest/app.c
+++ ctest/app.c
@@ -1,8 +1,14 @@
-//#include <stdint.h>
+#include <stdint.h>
+
+#define UART4_TDR 0x40010028
 
 void SystemInit(void) { return; }
 
 int main(void) {
+
+	*(char *)UART4_TDR = 'D';
+	*(char *)UART4_TDR = 'E';
+	*(char *)UART4_TDR = 'F';
 
 	while (1)
 		;
