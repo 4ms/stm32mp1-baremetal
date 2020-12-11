3082b4289c499440e82ba74b4a0b526210df3211
Dan Green <danngreen1@gmail.com>
2020-12-10 18:02:53 -0800

    testing function calls from main

--- ctest/app.c
+++ ctest/app.c
@@ -1,17 +1,31 @@
 #include <stdint.h>
 
 #define UART4_TDR 0x40010028
+volatile uint8_t *uart0 = (uint8_t *)UART4_TDR;
 
-void SystemInit(void) { return; }
+void write(const char *str) {
+	while (*str) {
+		*uart0 = *str++;
+	}
+}
 
-int main(void) {
+int main() {
+	*uart0 = 'X';
+	*uart0 = 'Y';
+	*uart0 = 'Z';
+	*uart0 = '\n';
 
-	*(char *)UART4_TDR = 'D';
-	*(char *)UART4_TDR = 'E';
-	*(char *)UART4_TDR = 'F';
-
-	while (1)
-		;
+	const char *s = "Hello world from bare-metal!\n";
+	write(s);
+	while (*s != '\0') {
+		*uart0 = *s;
+		s++;
+	}
+	while (1) {
+	};
 
 	return 0;
 }
+
+void SystemInit(void) { return; }
+
