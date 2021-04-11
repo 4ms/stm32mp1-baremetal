579f8c2f24e4fbfed1a103a370797b1f773b9a7f
Dan Green <danngreen1@gmail.com>
2021-04-11 14:49:45 -0700

    Added delays after writing to UART
    Added a softbreakpoint function

--- ctest/app.c
+++ ctest/app.c
@@ -4,19 +4,10 @@
 
 #include <stdint.h>
 
-#define UART4_TDR 0x40010028
-volatile uint8_t *uart0 = (uint8_t *)UART4_TDR;
 const uint32_t *UART4_ISR = (uint32_t *)0x40010028;
 const uint32_t TXFE = (1 << 23); // FIFO Empty
 const uint32_t TXFNF = (1 << 7); // TX FIFO Not Full
-const uint32_t TC = (1 << 6);    // Tx Complete
-
-// what is this?
-// #define GPIO1 0x4804C000
-// #define GPIO_OE 0x134
-/* .equ CM_PER, 0x44E00000 */
-/* .equ CM_PER_GPIO1_CLKCTRL, 0xAC */
-/* .equ GPIO_DATAOUT, 0x13C */
+const uint32_t TC = (1 << 6);	 // Tx Complete
 
 /* GPIO I, pin 8 = red D2 */
 /* GPIO I, pin 9 = green D2 */
@@ -25,90 +16,110 @@ const uint32_t TC = (1 << 6);    // Tx Complete
 /* all LEDs are active low */
 
 void delay_long() {
-  uint32_t i = 0x100000;
-  while (i--)
-    ;
+	uint32_t i = 0x100000;
+	while (i--)
+		;
+}
+
+void soft_breakpoint() {
+	volatile int stop = 1;
+	while (stop) {
+		// Attach a debugger and manually change the value at the address of `stop` in RAM from 1 to 0
+	}
 }
 
 void delay_for_uart(void) {
-  int i = 255;
-  while (i--)
-    ;
+	int i = 255;
+	while (i--)
+		;
 }
 
 uint32_t uart_ready() {
-  uint32_t isr = (*UART4_ISR);
-  if (isr & TC)
-    return 1;
-  else
-    return 0;
+	uint32_t isr = (*UART4_ISR);
+	if (isr & TC)
+		return 1;
+	else
+		return 0;
 }
 
 void write(const char *str) {
-  while (*str) {
-    UART4->TDR = *str++;
-    delay_for_uart();
-  }
+	while (*str) {
+		UART4->TDR = *str++;
+		delay_for_uart();
+	}
 }
 
 int main() {
-  UART4->TDR = 'X';
-  UART4->TDR = 'Y';
-  UART4->TDR = 'Z';
-  UART4->TDR = '\r';
-  UART4->TDR = '\n';
+	// Test UART
+	UART4->TDR = 'X';
+	delay_for_uart();
+	UART4->TDR = 'Y';
+	delay_for_uart();
+	UART4->TDR = 'Z';
+	delay_for_uart();
+	UART4->TDR = 'z';
+	delay_for_uart();
+	UART4->TDR = 'z';
+	delay_for_uart();
+	UART4->TDR = '\r';
+	delay_for_uart();
+	UART4->TDR = '\n';
+	delay_for_uart();
 
-  RCC->MC_AHB4ENSETR |= RCC_MC_AHB4ENSETR_GPIOIEN;
-  RCC->MC_AHB5ENSETR |= RCC_MC_AHB5ENSETR_GPIOZEN;
+	RCC->MC_AHB4ENSETR |= RCC_MC_AHB4ENSETR_GPIOIEN;
+	RCC->MC_AHB5ENSETR |= RCC_MC_AHB5ENSETR_GPIOZEN;
 
-  LL_GPIO_SetPinMode(GPIOI, LL_GPIO_PIN_8, LL_GPIO_MODE_OUTPUT);
-  LL_GPIO_SetPinOutputType(GPIOI, LL_GPIO_PIN_8, LL_GPIO_OUTPUT_PUSHPULL);
-  LL_GPIO_SetPinSpeed(GPIOI, LL_GPIO_PIN_8, LL_GPIO_SPEED_FREQ_MEDIUM);
-  LL_GPIO_SetPinMode(GPIOI, LL_GPIO_PIN_9, LL_GPIO_MODE_OUTPUT);
-  LL_GPIO_SetPinOutputType(GPIOI, LL_GPIO_PIN_9, LL_GPIO_OUTPUT_PUSHPULL);
-  LL_GPIO_SetPinSpeed(GPIOI, LL_GPIO_PIN_9, LL_GPIO_SPEED_FREQ_MEDIUM);
+	LL_GPIO_SetPinMode(GPIOI, LL_GPIO_PIN_8, LL_GPIO_MODE_OUTPUT);
+	LL_GPIO_SetPinOutputType(GPIOI, LL_GPIO_PIN_8, LL_GPIO_OUTPUT_PUSHPULL);
+	LL_GPIO_SetPinSpeed(GPIOI, LL_GPIO_PIN_8, LL_GPIO_SPEED_FREQ_MEDIUM);
+	LL_GPIO_SetPinMode(GPIOI, LL_GPIO_PIN_9, LL_GPIO_MODE_OUTPUT);
+	LL_GPIO_SetPinOutputType(GPIOI, LL_GPIO_PIN_9, LL_GPIO_OUTPUT_PUSHPULL);
+	LL_GPIO_SetPinSpeed(GPIOI, LL_GPIO_PIN_9, LL_GPIO_SPEED_FREQ_MEDIUM);
 
-  LL_GPIO_SetOutputPin(GPIOI, LL_GPIO_PIN_8);
-  LL_GPIO_ResetOutputPin(GPIOI, LL_GPIO_PIN_8);
+	LL_GPIO_SetOutputPin(GPIOI, LL_GPIO_PIN_8);
+	LL_GPIO_ResetOutputPin(GPIOI, LL_GPIO_PIN_8);
 
-  LL_GPIO_SetOutputPin(GPIOI, LL_GPIO_PIN_9);
-  LL_GPIO_ResetOutputPin(GPIOI, LL_GPIO_PIN_9);
+	LL_GPIO_SetOutputPin(GPIOI, LL_GPIO_PIN_9);
+	LL_GPIO_ResetOutputPin(GPIOI, LL_GPIO_PIN_9);
 
-  LL_GPIO_SetOutputPin(GPIOZ, LL_GPIO_PIN_6);
-  LL_GPIO_ResetOutputPin(GPIOZ, LL_GPIO_PIN_6);
+	LL_GPIO_SetOutputPin(GPIOZ, LL_GPIO_PIN_6);
+	LL_GPIO_ResetOutputPin(GPIOZ, LL_GPIO_PIN_6);
 
-  LL_GPIO_SetOutputPin(GPIOZ, LL_GPIO_PIN_7);
-  LL_GPIO_ResetOutputPin(GPIOZ, LL_GPIO_PIN_7);
+	LL_GPIO_SetOutputPin(GPIOZ, LL_GPIO_PIN_7);
+	LL_GPIO_ResetOutputPin(GPIOZ, LL_GPIO_PIN_7);
 
-  const char *s = "Hello world from bare-metal!\r\n";
-  write(s);
+	// Test function calls (tests the stack)
+	const char *s = "Hello world from bare-metal!\r\n";
+	write(s);
 
-  const char *s2 = "And hi to you too!\r\n";
-  while (*s2 != '\0') {
-    UART4->TDR = *s2;
-    s2++;
-    delay_for_uart();
-  }
+	const char *s2 = "And hi to you too!\r\n";
+	while (*s2 != '\0') {
+		UART4->TDR = *s2;
+		s2++;
+		delay_for_uart();
+	}
 
-  while (1) {
-    LL_GPIO_SetOutputPin(GPIOI, LL_GPIO_PIN_8);
-    delay_long();
-    LL_GPIO_ResetOutputPin(GPIOI, LL_GPIO_PIN_8);
+	while (1) {
+		LL_GPIO_SetOutputPin(GPIOI, LL_GPIO_PIN_8);
+		delay_long();
+		LL_GPIO_ResetOutputPin(GPIOI, LL_GPIO_PIN_8);
 
-    LL_GPIO_SetOutputPin(GPIOI, LL_GPIO_PIN_9);
-    delay_long();
-    LL_GPIO_ResetOutputPin(GPIOI, LL_GPIO_PIN_9);
+		LL_GPIO_SetOutputPin(GPIOI, LL_GPIO_PIN_9);
+		delay_long();
+		LL_GPIO_ResetOutputPin(GPIOI, LL_GPIO_PIN_9);
 
-    LL_GPIO_SetOutputPin(GPIOZ, LL_GPIO_PIN_6);
-    delay_long();
-    LL_GPIO_ResetOutputPin(GPIOZ, LL_GPIO_PIN_6);
+		LL_GPIO_SetOutputPin(GPIOZ, LL_GPIO_PIN_6);
+		delay_long();
+		LL_GPIO_ResetOutputPin(GPIOZ, LL_GPIO_PIN_6);
 
-    LL_GPIO_SetOutputPin(GPIOZ, LL_GPIO_PIN_7);
-    delay_long();
-    LL_GPIO_ResetOutputPin(GPIOZ, LL_GPIO_PIN_7);
-  };
+		LL_GPIO_SetOutputPin(GPIOZ, LL_GPIO_PIN_7);
+		delay_long();
+		LL_GPIO_ResetOutputPin(GPIOZ, LL_GPIO_PIN_7);
+	};
 
-  return 0;
+	return 0;
 }
 
-void SystemInit(void) { return; }
+void SystemInit(void) {
+	return;
+}
