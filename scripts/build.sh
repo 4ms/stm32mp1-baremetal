8bc8c1e8b16279cda4fe9c86ba32e0f7ceb498e1
Dan Green <danngreen1@gmail.com>
2021-04-19 20:39:41 -0700

    Renamed dir u-boot-stm32mp-2020.01-r0/ to u-boot/

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 
 echo "Usage: scripts/build.sh"
 
-cd u-boot-stm32mp1-baremetal
+cd u-boot/u-boot-stm32mp1-baremetal
 make O=../build CROSS_COMPILE=arm-none-eabi- stm32mp15x_baremetal_defconfig
 make -j16 O=../build CROSS_COMPILE=arm-none-eabi- all
-cd ..
+cd ../..
