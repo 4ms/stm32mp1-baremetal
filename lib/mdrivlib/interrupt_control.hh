8421dd1690cc6900fe52fa2ff0353189d994760d
Dan Green <danngreen1@gmail.com>
2021-05-07 22:33:39 -0700

    README for mdrivlib

--- lib/mdrivlib/interrupt_control.hh
+++ lib/mdrivlib/interrupt_control.hh
@@ -7,6 +7,16 @@ namespace mdrivlib
 struct InterruptControl {
 
 	//Sets the priority and sub-priority for an IRQ
+	//
+	// Note on priority:
+	// Binary Point is set to 4 in IRQ_Initialize().
+	// From Table 3-2 in arm_gic_architecture_specifiation, the group and sub-group are therefore:
+	//     ggg.sssss
+	// ARM CA7 uses 5-bit priority, and is left-aligned, so bottom 3 sss bits are ignored.
+	// So we have:
+	//     ggg.ssXXX
+	//
+	// So, pri1 is upper 3 bits (value can be 0-7), pri2 is lower 2 bits (value can be 0-3)
 	static void set_irq_priority(IRQn_Type irqn, uint32_t pri1, uint32_t pri2) {
 		auto pri = (std::clamp(pri1, 0UL, 7UL) << 5) | (std::clamp(pri2, 0UL, 3UL) << 3);
 		GIC_SetPriority(irqn, pri);
@@ -24,14 +34,5 @@ struct InterruptControl {
 		GIC_EnableIRQ(irqn);
 	}
 
-	// Note on priority:
-	// Binary Point is set to 4 in IRQ_Initialize().
-	// From Table 3-2 in arm_gic_architecture_specifiation, the group and sub-group are therefore:
-	//     ggg.sssss
-	// ARM CA7 uses 5-bit priority, and is left-aligned, so bottom 3 sss bits are ignored.
-	// So we have:
-	//     ggg.ssXXX
-	//
-	// So, pri1 is upper 3 bits (value can be 0-7), pri2 is lower 2 bits (value can be 0-3)
 };
 } // namespace mdrivlib
