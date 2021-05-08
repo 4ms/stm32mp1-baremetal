4d64eb80eb63e1d8b985aca0b2f63b054c27c6cb
Dan Green <danngreen1@gmail.com>
2021-05-08 14:19:28 -0700

    ctest cleanups: testing global static, slowed down LED animation
    Cleaned-up some comments

--- ctest/app.c
+++ ctest/app.c
@@ -9,14 +9,9 @@ void write(const char *str);
 static inline void delay_for_uart(void);
 static inline void init_leds();
 
-// OSD32MP1 board:
-// GPIO I, pin 8 = red D2
-// GPIO I, pin 9 = green D2
-// GPIO Z, pin 6 = red D1
-// GPIO Z, pin 7 = green D1
-// all LEDs are active low
+const char *global_constant_string = "And hi to you too!\r\n";
 
-int main() {
+void main() {
 	// Test UART
 	UART4->TDR = 'X';
 	delay_for_uart();
@@ -29,6 +24,7 @@ int main() {
 	UART4->TDR = '\n';
 	delay_for_uart();
 
+	// Test LEDs
 	init_leds();
 
 	LL_GPIO_SetOutputPin(GPIOI, LL_GPIO_PIN_8);
@@ -48,10 +44,10 @@ int main() {
 	const char *s = "Hello world from bare-metal!\r\n";
 	write(s);
 
-	const char *s2 = "And hi to you too!\r\n";
-	while (*s2 != '\0') {
-		UART4->TDR = *s2;
-		s2++;
+	// Test global static memory
+	while (*global_constant_string != '\0') {
+		UART4->TDR = *global_constant_string;
+		global_constant_string++;
 		delay_for_uart();
 	}
 
@@ -73,12 +69,10 @@ int main() {
 		delay_long();
 		LL_GPIO_ResetOutputPin(GPIOZ, LL_GPIO_PIN_7);
 	};
-
-	return 0;
 }
 
 void delay_long() {
-	uint32_t i = 0x100000;
+	uint32_t i = 0x1000000;
 	while (i--)
 		;
 }
@@ -90,23 +84,21 @@ void write(const char *str) {
 	}
 }
 
-// Handy utility when using a debugger
-void soft_breakpoint() {
-	volatile int stop = 1;
-	while (stop) {
-		// Attach a debugger and manually change the value at the address of `stop` in RAM from 1 to 0
-	}
-}
-
 // Inline because we don't want to assume function calling and returning works yet!
 static inline void delay_for_uart(void) {
-	int i = 255;
-	while (i--)
+	while ((UART4->ISR & USART_ISR_TXFT) == 0)
 		;
 }
 
 // Inline because we don't want to assume function calling and returning works yet!
 static inline void init_leds() {
+	// OSD32MP1 board:
+	// GPIO I, pin 8 = red D2
+	// GPIO I, pin 9 = green D2
+	// GPIO Z, pin 6 = red D1
+	// GPIO Z, pin 7 = green D1
+	// all LEDs are active low
+
 	// Enable RCC for GPIO I and GPIO Z (for the LEDs on the OSD32 board)
 	RCC->MC_AHB4ENSETR |= RCC_MC_AHB4ENSETR_GPIOIEN;
 	RCC->MC_AHB5ENSETR |= RCC_MC_AHB5ENSETR_GPIOZEN;
@@ -119,8 +111,10 @@ static inline void init_leds() {
 	LL_GPIO_SetPinSpeed(GPIOI, LL_GPIO_PIN_9, LL_GPIO_SPEED_FREQ_MEDIUM);
 }
 
-// Todo:
-// - Test IRQ handler: UART RX ISR, or HSEM ISR
-// - Test static initialization of cpp objects: create a data structure, then output it via the UART
-// - Test SysTick (do we have HAL_Tick()?)
-// - Get USE_HAL_DRIVER working
+// Handy utility when using a debugger
+void soft_breakpoint() {
+	volatile int stop = 1;
+	while (stop) {
+		// Attach a debugger and manually change the value at the address of `stop` in RAM from 1 to 0
+	}
+}
