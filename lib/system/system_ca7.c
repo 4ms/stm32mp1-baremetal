53e6f8f2b9ffb79bac992ce7ce66265c8013e62d
Dan Green <danngreen1@gmail.com>
2021-05-08 13:58:40 -0700

    Removed force-reset of all peripheral buses (APB1-4, AHB2, AHB4)

--- lib/system/system_ca7.c
+++ lib/system/system_ca7.c
@@ -8,24 +8,6 @@ void SystemInit(void)
 	// do not use global variables because this function is called before
 	// reaching pre-main. RW section may be overwritten afterwards.
 
-	RCC->APB1RSTSETR = 0xFFFFFFFF;
-	RCC->APB1RSTCLRR = 0xFFFFFFFF;
-
-	RCC->APB2RSTSETR = 0xFFFFFFFF;
-	RCC->APB2RSTCLRR = 0xFFFFFFFF;
-
-	RCC->APB3RSTSETR = 0xFFFFFFFF;
-	RCC->APB3RSTCLRR = 0xFFFFFFFF;
-
-	RCC->APB4RSTSETR = 0xFFFFFFFF;
-	RCC->APB4RSTCLRR = 0xFFFFFFFF;
-
-	RCC->AHB2RSTSETR = 0xFFFFFFFF;
-	RCC->AHB2RSTCLRR = 0xFFFFFFFF;
-
-	RCC->AHB4RSTSETR = 0xFFFFFFFF;
-	RCC->AHB4RSTCLRR = 0xFFFFFFFF;
-
 	// Invalidate entire Unified TLB
 	__set_TLBIALL(0);
 
@@ -41,7 +23,6 @@ void SystemInit(void)
 
 	L1C_InvalidateDCacheAll();
 
-	// #if ((__FPU_PRESENT == 1) && (__FPU_USED == 1))
 	__FPU_Enable();
 
 	MMU_CreateTranslationTable();
