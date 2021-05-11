4b18410696e8c0c6d488b5a9edcca2016cdac7fa
Dan Green <danngreen1@gmail.com>
2021-05-11 13:12:34 -0700

    Using HSEM for outer IRQ (WIP)

--- nested_irq/main.cc
+++ nested_irq/main.cc
@@ -53,11 +53,13 @@ void pin_change_isr_stop(uint8_t pin_num)
 constexpr uint8_t GPIO_I = 8; // GPIOA = 0, GPIOB = 1, etc..
 constexpr uint8_t red_led1_gpio = GPIO_I;
 constexpr uint8_t red_led1_pin = 8;
-constexpr auto red_led1_irqnum = EXTI8_IRQn;
+constexpr auto red_led1_irqnum = HSEM_IT1_IRQn;
+// constexpr auto red_led1_irqnum = EXTI8_IRQn;
 constexpr uint8_t green_led1_gpio = GPIO_I;
 constexpr uint8_t green_led1_pin = 9;
 constexpr auto green_led1_irqnum = EXTI9_IRQn;
 
+constexpr uint32_t SemaphoreID = 0;
 void main()
 {
 	Uart<UART4_BASE> uart;
@@ -83,15 +85,22 @@ void main()
 
 	// Outer interrupt = red LED 1 turning off
 	// EXTI pin-change interrupt on rising edge of PI8
-	enable_pin_change_isr(red_led1_gpio, red_led1_pin, true, false);
+	// enable_pin_change_isr(red_led1_gpio, red_led1_pin, true, false);
+
+	// enable unlock ISR
+	HSEM->C1IER = HSEM->C1IER | (1 << SemaphoreID);
+	// clear pending ISR
+	HSEM->C1ICR = 1 << SemaphoreID;
+	// lock
+	volatile uint32_t lock = HSEM->RLR[SemaphoreID];
+
 	InterruptManager::registerISR(red_led1_irqnum, [&]() {
 		// Spend a lot of time in here because while it's running,
 		// the inner ISR needs to be triggered, be handled, and return
 		uart.write("2) Entering red LED 1 ISR (Outer)\r\n");
 		for (int i = 0; i < 800; i++) {
 			if (i == 50) {
-				// Trigger the inner ISR
-				uart.write("3) Green LED 1 turning off (PI9 rising edge)\r\n");
+				uart.write("3) Green LED 1 turning off (ISR for PI9 rising edge)\r\n");
 				green_led1.off();
 			}
 			red_led2.on(); // This prevents optimizer from getting rid of the loop
@@ -100,33 +109,40 @@ void main()
 		uart.write("6) Exiting red LED 1 ISR (Outer)\r\n");
 	});
 
-	// Set to low priority (so it can be interrupted by the inner interrupt)
-	GIC_SetPriority(red_led1_irqnum, 0b11111000);
+	// // Set to low priority (so it can be interrupted by the inner interrupt)
+	GIC_SetTarget(red_led1_irqnum, 1);
+	GIC_SetPriority(red_led1_irqnum, 0b10000000);
 	GIC_SetConfiguration(red_led1_irqnum, 0b10); // Edge triggered
 
 	// Inner interrupt = green LED 1 turning off
 	// EXTI pin-change interrupt on rising edge of PI9 (green_led1 turning off)
 	enable_pin_change_isr(green_led1_gpio, green_led1_pin, true, false);
 	InterruptManager::registerISR(green_led1_irqnum, [&]() {
-		uart.write("4"); //) Entering green LED 1 ISR (Outer)\r\n");
 		clear_falling_pin_change_isr_flag(green_led1_pin);
-		uart.write("5"); //) Exiting green LED 1 ISR (Outer)\r\n");
+		uart.write("4) Entering green LED 1 ISR (Outer)\r\n");
+		green_led2.on();
+		uart.write("5) Exiting green LED 1 ISR (Outer)\r\n");
 	});
 
 	// Set to high priority so it can interrupt the outer interrupt
+	GIC_SetTarget(green_led1_irqnum, 1);
 	GIC_SetPriority(green_led1_irqnum, 0b00000000);
 	GIC_SetConfiguration(green_led1_irqnum, 0b10); // Edge triggered
 
 	// Enable everything
-	pin_change_isr_enable(red_led1_pin);
+	// pin_change_isr_enable(red_led1_pin);
 	pin_change_isr_enable(green_led1_pin);
 	GIC_EnableIRQ(red_led1_irqnum);
 	GIC_EnableIRQ(green_led1_irqnum);
 
 	// Trigger the ISRs
 	uart.write("1) Red LED 1 turning off (PI8 rising edge)\r\n");
-	red_led1.off();
+	// red_led1.off();
+
+	// unlock
+	HSEM->R[SemaphoreID] = HSEM_CR_COREID_CURRENT;
 
 	while (1) {
+		__WFI();
 	};
 }
