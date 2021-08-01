e548ca8017bce05b2985032d9325f5da20f16a2b
Dan Green <danngreen1@gmail.com>
2021-08-01 16:39:12 -0700

    AudioStream measures and records processor load

--- examples/audio_processor/audio.hh
+++ examples/audio_processor/audio.hh
@@ -2,6 +2,7 @@
 #include "audio_codec_conf.hh"
 #include "audio_stream_conf.hh"
 #include "drivers/codec_CS42L51.hh"
+#include "drivers/cycle_counter.hh"
 #include "drivers/i2c.hh"
 
 using mdrivlib::CodecCS42L51;
@@ -31,13 +32,25 @@ public:
 		codec.init();
 		codec.set_rx_buffers(audio_in_dma_block);
 		codec.set_tx_buffers(audio_out_dma_block);
+		load_measurer.init();
 	}
 
 	void set_process_function(AudioProcessFunction &&process)
 	{
 		_process = std::move(process);
-		codec.set_callbacks([this] { _process(audio_in_dma_block[0], audio_out_dma_block[0]); },
-							[this] { _process(audio_in_dma_block[1], audio_out_dma_block[1]); });
+		codec.set_callbacks(
+			[this] {
+				audio_load = load_measurer.get_last_measurement_load_percent();
+				load_measurer.start_measurement();
+				_process(audio_in_dma_block[0], audio_out_dma_block[0]);
+				load_measurer.end_measurement();
+			},
+			[this] {
+				audio_load = load_measurer.get_last_measurement_load_percent();
+				load_measurer.start_measurement();
+				_process(audio_in_dma_block[1], audio_out_dma_block[1]);
+				load_measurer.end_measurement();
+			});
 	}
 
 	void start()
@@ -45,6 +58,13 @@ public:
 		codec.start();
 	}
 
+	uint32_t get_load_measurement()
+	{
+		return audio_load;
+	}
+
 private:
 	AudioProcessFunction _process;
+	mdrivlib::CycleCounter load_measurer;
+	uint32_t audio_load;
 };
