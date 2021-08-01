0e5406266e324e0b5e5dc9946b76e68b317d2c43
Dan Green <danngreen1@gmail.com>
2021-08-01 16:39:52 -0700

    audio_proc: Buttons cycle through FX, and print processor load

--- examples/audio_processor/main.cc
+++ examples/audio_processor/main.cc
@@ -1,10 +1,10 @@
 #include "audio.hh"
 #include "drivers/stm32xx.h"
 #include "dual_fm_osc.hh"
-#include "shared/disco_conf.hh"
 #include "shared/drivers/delay.hh"
 #include "shared/drivers/leds.hh"
 #include "shared/drivers/uart.hh"
+#include "shared/stm32disco_conf.hh"
 #include "util/math.hh"
 #include "util/oscs.hh"
 #include "util/zip.hh"
@@ -15,16 +15,20 @@ using AudioOutBuffer = AudioStreamConf::AudioOutBuffer;
 
 void main()
 {
-	// UART
+	// UI
 	Uart<UART4_BASE> uart;
 	uart.write("\r\n\r\nStarting Audio Processor\r\n");
 
 	STM32MP1Disco::GreenLED green_led;
 	STM32MP1Disco::RedLED red_led;
+	STM32MP1Disco::User1Button button1;
+	STM32MP1Disco::User2Button button2;
 	red_led.on();
 	green_led.on();
 
-	AudioStream audio;
+	// FX
+	enum class FX { Passthrough, DualFMOsc, EndOfList };
+	FX current_fx = FX::Passthrough;
 
 	auto passthrough = [](AudioInBuffer &in_buffer, AudioOutBuffer &out_buffer) {
 		for (auto [in, out] : zip(in_buffer, out_buffer)) {
@@ -34,33 +38,45 @@ void main()
 	};
 
 	DualFMOsc<AudioStreamConf> dual_fm_osc;
-
 	auto dual_fm_osc_process = [&dual_fm_osc](AudioInBuffer &in_buffer, AudioOutBuffer &out_buffer) {
 		dual_fm_osc.process(in_buffer, out_buffer);
 	};
 
-	// Select one of these:
+	// AudioStream
+	AudioStream audio;
 	audio.set_process_function(passthrough);
-	audio.set_process_function(dual_fm_osc_process);
-
 	audio.start();
 
 	while (true) {
 
-		red_led.on();
-		Delay::cycles(5000000);
-		red_led.off();
+		if (button1.is_just_pressed()) {
+			switch (current_fx) {
+				case FX::Passthrough:
+					current_fx = FX::DualFMOsc;
+					audio.set_process_function(passthrough);
+					uart.write("Using DualFMOsc FX\r\n");
+					red_led.on();
+					green_led.off();
+					break;
 
-		green_led.on();
-		Delay::cycles(5000000);
-		green_led.off();
+				case FX::DualFMOsc:
+					current_fx = FX::Passthrough;
+					audio.set_process_function(dual_fm_osc_process);
+					uart.write("Using Passthrough FX\r\n");
+					red_led.off();
+					green_led.on();
+					break;
 
-		// blink the lights
-		//
-		// switch Processor if button0 pressed
-		//
-		// print out the processor load if button1 pressed
-		//
+				default:
+					break;
+			}
+		}
+
+		if (button2.is_just_pressed()) {
+			uart.write("Current load: ");
+			uart.write(audio.get_load_measurement());
+			uart.write("\r\n");
+		}
 	}
 }
 
