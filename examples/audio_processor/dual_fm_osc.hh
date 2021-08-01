c90df8bfc262bdcc566f8d5f98bf9418961d6708
Dan Green <danngreen1@gmail.com>
2021-07-31 17:14:27 -0700

    Added DualFMOsc class

---
+++ examples/audio_processor/dual_fm_osc.hh
@@ -0,0 +1,34 @@
+#pragma once
+#include "util/math.hh"
+#include "util/math_tables.hh"
+#include "util/oscs.hh"
+
+template<typename AudioStreamConf>
+class DualFMOsc {
+	DualFMOsc() = delete;
+	static inline TriangleOscillator<48000> modulator;
+	static inline TriangleOscillator<48000> carrier;
+
+	using AudioInBuffer = typename AudioStreamConf::AudioInBuffer;
+	using AudioOutBuffer = typename AudioStreamConf::AudioOutBuffer;
+
+public:
+	static void process(AudioInBuffer &in_buffer, AudioOutBuffer &out_buffer)
+	{
+		uint32_t freq_control_sum = 0;
+		for (auto in : in_buffer)
+			freq_control_sum += AudioStreamConf::AudioInFrame::scaleInput(in.chan[0]);
+		float freq_control_avg = freq_control_sum / (float)AudioStreamConf::BlockSize;
+
+		float freq_mult = exp5Table.interp(MathTools::constrain(freq_control_avg / 2.f + 0.5f, 0.f, 1.0f));
+		modulator.set_frequency(80 * freq_mult);
+
+		for (auto &out : out_buffer) {
+			float mod_output = modulator.process_float();
+			out.chan[0] = AudioStreamConf::AudioOutFrame::scaleOutput(mod_output);
+
+			carrier.set_frequency(20 * mod_output);
+			out.chan[1] = AudioStreamConf::AudioOutFrame::scaleOutput(carrier.process_float());
+		}
+	};
+};
