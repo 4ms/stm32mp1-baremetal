f223241b4f8d4505024afe4bb321023ff6f76afa
Dan Green <danngreen1@gmail.com>
2021-08-02 16:32:27 -0700

    OSD32 Breakout conf: LED naming compatible with Disco board

--- examples/shared/osd32brk_conf.hh
+++ examples/shared/osd32brk_conf.hh
@@ -2,11 +2,12 @@
 #include "drivers/leds.hh"
 #include "lib/mdrivlib/drivers/debounced_switch.hh"
 
-namespace OSD32BRK {
+namespace OSD32BRK
+{
 
-using RedLED1 = Led<GPIOZ_BASE, 6, LedActive::Low>;
-using GreenLED1 = Led<GPIOZ_BASE, 7, LedActive::Low>;
-using RedLED2 = Led<GPIOI_BASE, 8, LedActive::Low> ;
-using GreenLED2 = Led<GPIOI_BASE, 9, LedActive::Low> ;
+using RedLED = Led<GPIOZ_BASE, 6, LedActive::Low>;
+using GreenLED = Led<GPIOZ_BASE, 7, LedActive::Low>;
+using RedLED2 = Led<GPIOI_BASE, 8, LedActive::Low>;
+using GreenLED2 = Led<GPIOI_BASE, 9, LedActive::Low>;
 
-}
+} // namespace OSD32BRK
