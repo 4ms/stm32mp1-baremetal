f9309441c2b0fa2f6f4643d34f7c1c4094663bc4
Dan Green <danngreen1@gmail.com>
2021-08-03 11:57:24 -0700

    Cooler Dual FM Osc effect

--- examples/audio_processor/dual_fm_osc.hh
+++ examples/audio_processor/dual_fm_osc.hh
@@ -5,8 +5,9 @@
 
 template<typename AudioStreamConf>
 class DualFMOsc {
-	TriangleOscillator<48000> modulator;
-	TriangleOscillator<48000> carrier;
+	RampOscillator<48000> ramp{1};
+	TriangleOscillator<48000> modulator{40};
+	SineOscillator<48000> carrier{250};
 
 	using AudioInBuffer = typename AudioStreamConf::AudioInBuffer;
 	using AudioOutBuffer = typename AudioStreamConf::AudioOutBuffer;
@@ -14,20 +15,16 @@ class DualFMOsc {
 public:
 	void process(AudioInBuffer &in_buffer, AudioOutBuffer &out_buffer)
 	{
-		uint32_t freq_control_sum = 0;
-		for (auto in : in_buffer)
-			freq_control_sum += AudioStreamConf::AudioInFrame::scaleInput(in.chan[0]);
-		float freq_control_avg = freq_control_sum / (float)AudioStreamConf::BlockSize;
-
-		float freq_mult = exp5Table.interp(MathTools::constrain(freq_control_avg / 2.f + 0.5f, 0.f, 1.0f));
-		modulator.set_frequency(80 * freq_mult);
-
 		for (auto &out : out_buffer) {
-			float mod_output = modulator.process_float();
-			out.chan[0] = AudioStreamConf::AudioOutFrame::scaleOutput(mod_output);
+			float freq_mult_1 = exp5Table.interp(ramp.process_float());
+			modulator.set_frequency(40 * freq_mult_1);
 
-			carrier.set_frequency(20 * mod_output);
-			out.chan[1] = AudioStreamConf::AudioOutFrame::scaleOutput(carrier.process_float());
+			float freq_mult_2 = exp5Table.interp(modulator.process_float());
+			carrier.set_frequency(20 * freq_mult_2);
+			carrier.process();
+
+			out.chan[0] = modulator.val() >> 8;
+			out.chan[1] = carrier.val() >> 8;
 		}
 	};
 };
