0771f488157f66928818d485353ca9d055c2b80e
Dan Green <danngreen1@gmail.com>
2021-08-03 12:12:04 -0700

    Cleaned up audio stream (removed duplicated code)

--- examples/audio_processor/audio_stream.hh
+++ examples/audio_processor/audio_stream.hh
@@ -1,10 +1,10 @@
 #pragma once
 #include "audio_codec_conf.hh"
 #include "audio_stream_conf.hh"
+#include "drivers/cache.hh"
 #include "drivers/codec_CS42L51.hh"
 #include "drivers/cycle_counter.hh"
 #include "drivers/i2c.hh"
-#include "drivers/cache.hh"
 
 using AudioInBuffer = AudioStreamConf::AudioInBuffer;
 using AudioOutBuffer = AudioStreamConf::AudioOutBuffer;
@@ -15,13 +15,12 @@ class AudioStream {
 	mdrivlib::I2CPeriph i2c;
 	mdrivlib::CodecCS42L51 codec;
 
-	// These must be in a region of RAM that's not cached
-	// -- OR else we have to invalidate the cache before we use incoming DMA data
-	// and clean it after processing and writing to the outgoing DMA buffer
-	// static inline __attribute__((section(".noncachable"))) AudioInBlock audio_in_dma_block;
-	// static inline __attribute__((section(".noncachable"))) AudioOutBlock audio_out_dma_block;
-	 static inline AudioInBlock audio_in_dma_block;
-	 static inline AudioOutBlock audio_out_dma_block;
+	// DMA buffers must be in a region of RAM that's not cached
+	// -- OR -- we have to use cache maintenance routines
+	// (That is, invalidate the cache before reading the incoming DMA buffer
+	// and clean the cache after writing to the outgoing DMA buffer)
+	static inline __attribute__((section(".noncachable"))) AudioInBlock audio_in_dma_block;
+	static inline __attribute__((section(".noncachable"))) AudioOutBlock audio_out_dma_block;
 
 public:
 	using AudioProcessFunction = std::function<void(AudioInBuffer &, AudioOutBuffer &)>;
@@ -34,36 +33,12 @@ public:
 		codec.set_rx_buffers(audio_in_dma_block[0]);
 		codec.set_tx_buffers(audio_out_dma_block[0]);
 		load_measurer.init();
-		for (auto &out_buffer : audio_out_dma_block) {
-			for (auto &out : out_buffer) {
-				out.chan[0] = 0xAAAAAA;
-				out.chan[1] = 0x111111;
-			}
-		}
-
-		mdrivlib::SystemCache::clean_dcache();	
 	}
 
 	void set_process_function(AudioProcessFunction &&process)
 	{
-		_process = std::move(process);
-		codec.set_callbacks(
-			[this] {
-				audio_load = load_measurer.get_last_measurement_load_percent();
-				load_measurer.start_measurement();
-				mdrivlib::SystemCache::invalidate_dcache_by_range(audio_in_dma_block[1].data(), sizeof(AudioInBuffer));	
-				_process(audio_in_dma_block[1], audio_out_dma_block[1]);
-				mdrivlib::SystemCache::clean_dcache_by_range(audio_out_dma_block[1].data(), sizeof(AudioOutBuffer));	
-				load_measurer.end_measurement();
-			},
-			[this] {
-				audio_load = load_measurer.get_last_measurement_load_percent();
-				load_measurer.start_measurement();
-				mdrivlib::SystemCache::invalidate_dcache_by_range(audio_in_dma_block[0].data(), sizeof(AudioInBuffer));	
-				_process(audio_in_dma_block[0], audio_out_dma_block[0]);
-				mdrivlib::SystemCache::clean_dcache_by_range(audio_out_dma_block[0].data(), sizeof(AudioOutBuffer));	
-				load_measurer.end_measurement();
-			});
+		_process_func = std::move(process);
+		codec.set_callbacks([this] { _process<1>(); }, [this] { _process<0>(); });
 	}
 
 	void start()
@@ -76,8 +51,17 @@ public:
 		return audio_load;
 	}
 
+	template<uint32_t buffer_half>
+	void _process()
+	{
+		audio_load = load_measurer.get_last_measurement_load_percent();
+		load_measurer.start_measurement();
+		_process_func(audio_in_dma_block[buffer_half], audio_out_dma_block[buffer_half]);
+		load_measurer.end_measurement();
+	}
+
 private:
-	AudioProcessFunction _process;
+	AudioProcessFunction _process_func;
 	mdrivlib::CycleCounter load_measurer;
 	uint32_t audio_load;
 };
