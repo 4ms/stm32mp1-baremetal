a03d2b868ca37f2bb605942b2aceb1aa5e367bea
Dan Green <danngreen1@gmail.com>
2021-09-14 19:58:50 -0700

    Moved CMakeLists.txt to project dir, and created common cmake file for all projects

---
+++ cmake/common.cmake
@@ -0,0 +1,34 @@
+enable_language(C CXX ASM)
+set(CMAKE_C_STANDARD 11)
+set(CMAKE_C_STANDARD_REQUIRED ON)
+set(CMAKE_C_EXTENSIONS ON)
+set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_EXTENSIONS ON)
+
+set(EXECUTABLE ${PROJECT_NAME}.elf)
+
+# Directories with source files shared between projects
+set(EXTLIB_DIR ../../third-party)
+set(SHARED_DIR ../shared)
+set(UBOOT_DIR ../../third-party/u-boot/build)
+
+# where uimg will be loaded and entered
+set(LOADADDR 0xC2000040)
+set(ENTRYPOINT 0xC2000040)
+
+# Defines and flags
+set(default_defines
+	-DUSE_FULL_LL_DRIVER
+	-DSTM32MP157Cxx
+	-DSTM32MP1
+	-DCORE_CA7
+	)
+
+set(MCU_FLAGS 
+ 	-mcpu=cortex-a7
+	-march=armv7ve
+	-mfpu=neon-vfpv4
+	-mlittle-endian
+	-mfloat-abi=hard
+	)
---
+++ test.sh
@@ -0,0 +1,1 @@
+rm -rf build && ./build.sh && cd examples/ctest && rm -rf build/ && ./build.sh && cd ../.. && ls -l build/examples/ctest && ls -l examples/ctest/build
