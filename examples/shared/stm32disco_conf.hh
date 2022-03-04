f91c0e0a32c22e65f02f486cc58a63dd1aa49718
Dan Green <danngreen1@gmail.com>
2022-03-04 12:26:40 -0800

    Split stm32disco conf into a file each for buttons and leds.
    Since buttons depends on a driver, we might want to just use LEDs and reduce dependencies.

--- examples/shared/stm32disco_conf.hh
+++ examples/shared/stm32disco_conf.hh
@@ -1,10 +1,8 @@
 #pragma once
 #include "drivers/leds.hh"
-#include "lib/mdrivlib/drivers/debounced_switch.hh"
 
 namespace STM32MP1Disco
 {
-
 using OrangeLED = Led<GPIOH_BASE, 7, LedActive::High>;
 using LD7 = OrangeLED;
 
@@ -18,7 +16,7 @@ using LD5 = GreenLED;
 using RedLED = Led<GPIOA_BASE, 13, LedActive::High>;
 using LD6 = RedLED;
 
-using User1Button = mdrivlib::DebouncedPin<mdrivlib::GPIO::A, 14, mdrivlib::PinPolarity::Inverted>;
-using User2Button = mdrivlib::DebouncedPin<mdrivlib::GPIO::A, 13, mdrivlib::PinPolarity::Inverted>;
+using RedLED2 = OrangeLED; //For compatibility with OSD32BRK board
+using GreenLED2 = BlueLED; //For compatibility with OSD32BRK board
 
 } // namespace STM32MP1Disco
