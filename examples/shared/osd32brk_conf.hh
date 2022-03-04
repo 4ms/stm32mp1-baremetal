77890e170b518ac6c5926cb6d2dff876581e4686
Dan Green <danngreen1@gmail.com>
2022-03-04 12:26:55 -0800

    osd32conf gets aliases to be compatible with Discovery boards

--- examples/shared/osd32brk_conf.hh
+++ examples/shared/osd32brk_conf.hh
@@ -9,4 +9,6 @@ using GreenLED = Led<GPIOZ_BASE, 7, LedActive::Low>;
 using RedLED2 = Led<GPIOI_BASE, 8, LedActive::Low>;
 using GreenLED2 = Led<GPIOI_BASE, 9, LedActive::Low>;
 
+using OrangeLED = RedLED2; //For compatibility with STM32MP1 Discovery board
+using BlueLED = GreenLED2; //For compatibility with STM32MP1 Discovery board
 } // namespace OSD32BRK
