68f079e4cfc90d8c888dc7f1d5ee40b5536400c8
Dan Green <danngreen1@gmail.com>
2022-06-22 15:37:24 -0700

    Started FSBL example (replacement for u-boot) Got to blinky, that's all so far.

---
+++ examples/shared/fsbl/main.cc
@@ -0,0 +1,40 @@
+#include "drivers/leds.hh"
+#include "osd32brk_conf.hh"
+#include "stm32mp157cxx_ca7.h"
+#include <cstdint>
+
+using namespace OSD32BRK;
+void delay(unsigned cycles);
+
+void main()
+{
+	RedLED red_led;
+	GreenLED green_led;
+	OrangeLED blue_led;
+
+	red_led.init();
+	green_led.init();
+	blue_led.init();
+
+	constexpr uint32_t dlytime = 600000;
+	while (1) {
+		red_led.on();
+		delay(dlytime);
+		red_led.off();
+
+		green_led.on();
+		delay(dlytime);
+		green_led.off();
+
+		blue_led.on();
+		delay(dlytime);
+		blue_led.off();
+	}
+}
+
+void delay(unsigned cycles)
+{
+	while (cycles--) {
+		asm("nop");
+	}
+}
---
+++ examples/shared/fsbl/systeminit.c
@@ -0,0 +1,31 @@
+#include "stm32mp1xx.h"
+
+void SystemInit(void)
+{
+	// do not use global variables because this function is called before
+	// reaching pre-main. RW section may be overwritten afterwards.
+
+	// Invalidate entire Unified TLB
+	__set_TLBIALL(0);
+
+	// Invalidate entire branch predictor array
+	__set_BPIALL(0);
+	__DSB();
+	__ISB();
+
+	//  Invalidate instruction cache and flush branch target cache
+	__set_ICIALLU(0);
+	__DSB();
+	__ISB();
+
+	L1C_InvalidateDCacheAll();
+
+	__FPU_Enable();
+
+	L1C_EnableCaches();
+	L1C_EnableBTAC();
+
+#if (__L2C_PRESENT == 1)
+	L2C_Enable();
+#endif
+}
