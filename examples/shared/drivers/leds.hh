e8ae22de28bd6b60adae703f6dffb2b8e9a4c8f2
Dan Green <danngreen1@gmail.com>
2022-06-22 22:36:01 -0700

    Led class uses simple Pin class [push to main]

--- examples/shared/drivers/leds.hh
+++ examples/shared/drivers/leds.hh
@@ -1,4 +1,5 @@
 #pragma once
+#include "drivers/pin.hh"
 #include "stm32mp1xx.h"
 #include "stm32mp1xx_ll_gpio.h"
 
@@ -9,7 +10,7 @@ enum class LedActive { Low, High };
 // GPIO_BASE_ADDR: the base address of the GPIO peripheral. Typically you'll use something like GPIOA_BASE or
 // GPIOD_BASE. PIN_NUM: this is the pin number, from 0 to 15 POLARITY: either LedActive::Low or LedActive::High
 // (default). It selects whether the LED turns on when the pin is low or high.
-template<uint32_t GPIO_BASE_ADDR, uint32_t PIN_NUM, LedActive POLARITY = LedActive::High>
+template<GPIO GPIO_BASE_ADDR, uint32_t PIN_NUM, LedActive POLARITY = LedActive::High>
 class Led {
 	GPIO_TypeDef *const gpio;
 	const uint16_t pin_bit;
@@ -19,38 +20,8 @@ public:
 		: gpio{reinterpret_cast<GPIO_TypeDef *>(GPIO_BASE_ADDR)}
 		, pin_bit{1 << PIN_NUM}
 	{
-		init();
-	}
-
-	// (Re-)initializes the GPIO to make the pin an output
-	void init()
-	{
-		if constexpr (GPIO_BASE_ADDR == GPIOA_BASE)
-			RCC->MP_AHB4ENSETR = RCC_MC_AHB4ENSETR_GPIOAEN;
-		if constexpr (GPIO_BASE_ADDR == GPIOB_BASE)
-			RCC->MP_AHB4ENSETR = RCC_MC_AHB4ENSETR_GPIOBEN;
-		if constexpr (GPIO_BASE_ADDR == GPIOC_BASE)
-			RCC->MP_AHB4ENSETR = RCC_MC_AHB4ENSETR_GPIOCEN;
-		if constexpr (GPIO_BASE_ADDR == GPIOD_BASE)
-			RCC->MP_AHB4ENSETR = RCC_MC_AHB4ENSETR_GPIODEN;
-		if constexpr (GPIO_BASE_ADDR == GPIOE_BASE)
-			RCC->MP_AHB4ENSETR = RCC_MC_AHB4ENSETR_GPIOEEN;
-		if constexpr (GPIO_BASE_ADDR == GPIOF_BASE)
-			RCC->MP_AHB4ENSETR = RCC_MC_AHB4ENSETR_GPIOFEN;
-		if constexpr (GPIO_BASE_ADDR == GPIOH_BASE)
-			RCC->MP_AHB4ENSETR = RCC_MC_AHB4ENSETR_GPIOHEN;
-		if constexpr (GPIO_BASE_ADDR == GPIOI_BASE)
-			RCC->MP_AHB4ENSETR = RCC_MC_AHB4ENSETR_GPIOIEN;
-		if constexpr (GPIO_BASE_ADDR == GPIOJ_BASE)
-			RCC->MP_AHB4ENSETR = RCC_MC_AHB4ENSETR_GPIOJEN;
-		if constexpr (GPIO_BASE_ADDR == GPIOJ_BASE)
-			RCC->MP_AHB4ENSETR = RCC_MC_AHB4ENSETR_GPIOJEN;
-		if constexpr (GPIO_BASE_ADDR == GPIOZ_BASE)
-			RCC->MP_AHB5ENSETR = RCC_MC_AHB5ENSETR_GPIOZEN;
-
-		LL_GPIO_SetPinMode(gpio, pin_bit, LL_GPIO_MODE_OUTPUT);
-		LL_GPIO_SetPinOutputType(gpio, pin_bit, LL_GPIO_OUTPUT_PUSHPULL);
-		LL_GPIO_SetPinSpeed(gpio, pin_bit, LL_GPIO_SPEED_FREQ_MEDIUM);
+		PinConf pin{GPIO_BASE_ADDR, PIN_NUM, PinAF::AFNone};
+		pin.init(PinMode::Output);
 	}
 
 	void on()
--- examples/shared/osd32brk_conf.hh
+++ examples/shared/osd32brk_conf.hh
@@ -4,11 +4,11 @@
 namespace OSD32BRK
 {
 
-using RedLED = Led<GPIOZ_BASE, 6, LedActive::Low>;
-using GreenLED = Led<GPIOZ_BASE, 7, LedActive::Low>;
-using RedLED2 = Led<GPIOI_BASE, 8, LedActive::Low>;
-using GreenLED2 = Led<GPIOI_BASE, 9, LedActive::Low>;
+using RedLED = Led<GPIO::Z, 6, LedActive::Low>;
+using GreenLED = Led<GPIO::Z, 7, LedActive::Low>;
+using RedLED2 = Led<GPIO::I, 8, LedActive::Low>;
+using GreenLED2 = Led<GPIO::I, 9, LedActive::Low>;
 
-using OrangeLED = RedLED2; //For compatibility with STM32MP1 Discovery board
-using BlueLED = GreenLED2; //For compatibility with STM32MP1 Discovery board
+using OrangeLED = RedLED2; // For compatibility with STM32MP1 Discovery board
+using BlueLED = GreenLED2; // For compatibility with STM32MP1 Discovery board
 } // namespace OSD32BRK
