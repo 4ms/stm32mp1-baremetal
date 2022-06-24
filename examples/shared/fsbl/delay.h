f1b644913f410abbe10a9fe9c58b1fe5ef36dc88
Dan Green <danngreen1@gmail.com>
2022-06-24 14:32:52 -0700

    Fixed udelay

--- examples/shared/fsbl/delay.h
+++ examples/shared/fsbl/delay.h
@@ -1,9 +1,15 @@
+#pragma once
 #include <stdint.h>
 
 static inline void udelay(uint32_t usec)
 {
-	const unsigned long cycles = 120UL * 800000000UL * usec / 1000UL;
-	volatile unsigned long t = cycles;
+	// at -O0 the loop is 6 cycles: 2 loads, 1 store, 1 branch, 1 sub, 1 cmp
+	// So if usec=2 = 1.10us, and usec=10 = 2.87us,
+	// then 8 loops = 48cycles is 1.77us, so 1 cycle = 36.87ns so clock is at 27MHz (assuming SYSRAM has no
+	// wait states)
+	const unsigned long loops_per_us = 5;
+	const unsigned long loops = (unsigned long)usec * loops_per_us;
+	volatile unsigned long t = loops;
 	while (t)
 		t = t - 1;
 }
