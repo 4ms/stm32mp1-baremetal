e221e4a9c701927043f0dd54f9b3d72cf9b7ccc0
Dan Green <danngreen1@gmail.com>
2022-06-23 17:31:13 -0700

    Fix panic()

--- examples/shared/fsbl/ddr/print_messages.h
+++ examples/shared/fsbl/ddr/print_messages.h
@@ -3,6 +3,8 @@
 #define debug(...) printf_(__VA_ARGS__)
 #define pr_err(...) printf_(__VA_ARGS__)
 #define panic(...)                                                                                                     \
-	printf_(__VA_ARGS__);                                                                                              \
-	while (1)                                                                                                          \
-		;
+	do {                                                                                                               \
+		printf_(__VA_ARGS__);                                                                                          \
+		while (1)                                                                                                      \
+			;                                                                                                          \
+	} while (0);
