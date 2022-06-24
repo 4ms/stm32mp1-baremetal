dbaa01913532ca574514dd2a82e46112a3291cbb
Dan Green <danngreen1@gmail.com>
2022-06-23 17:30:58 -0700

    Test initing RAM

--- examples/shared/fsbl/main.cc
+++ examples/shared/fsbl/main.cc
@@ -1,3 +1,6 @@
+extern "C" {
+#include "ddr/stm32mp1_ram.h"
+}
 #include "drivers/leds.hh"
 #include "drivers/uart.hh"
 #include "osd32brk_conf.hh"
@@ -19,21 +22,33 @@ void main()
 	Uart<Board::ConsoleUART> uart(Board::UartRX, Board::UartTX, 115200);
 	uart.write("Starting FSBL...\n");
 	printf_("Printf works\n");
-	printf_("1 + 1 = %d", (1 + 1));
+	printf_("1 + 2 = %d\n", (1 + 2));
+
+	red_led.off();
+	red_led.on();
+	red_led.off();
+
+	int err = stm32mp1_ddr_setup();
+	printf_("Result = %d\n", err);
+
+	red_led.on();
+	if (err) {
+		blue_led.on();
+	}
 
 	constexpr uint32_t dlytime = 600000;
 	while (1) {
-		red_led.on();
-		delay(dlytime);
-		red_led.off();
+		// red_led.on();
+		// delay(dlytime);
+		// red_led.off();
 
 		green_led.on();
 		delay(dlytime);
 		green_led.off();
 
-		blue_led.on();
-		delay(dlytime);
-		blue_led.off();
+		// blue_led.on();
+		// delay(dlytime);
+		// blue_led.off();
 	}
 }
 
