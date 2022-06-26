8b2805cb334ed4273f0e014736d1d61c01b95675
Dan Green <danngreen1@gmail.com>
2022-06-25 18:14:00 -0700

    QSPI NOR flash driver only performs SIO and QIO reads
    init, write, erase might be implemented later, but aren't needed for bootloader

--- examples/shared/fsbl/norflash/qspi_nor.h
+++ examples/shared/fsbl/norflash/qspi_nor.h
@@ -5,8 +5,7 @@
 extern "C" {
 #endif
 
-uint32_t QSPI_init(void);
-uint32_t QSPI_read(uint8_t *pData, uint32_t read_addr, uint32_t num_bytes);
+uint32_t QSPI_read_SIO(uint8_t *pData, uint32_t read_addr, uint32_t num_bytes);
 
 #ifdef __cplusplus
 }
