bc0fae701b7839bd0d203cc5882568d2e2507586
Dan Green <danngreen1@gmail.com>
2022-06-27 15:11:48 -0700

    Refactored BootMediaLoader:
    not using spl_image, just storing what we need in _image_info
    Each step runs required previous steps if they haven't been called yet

--- examples/shared/fsbl/boot_media_loader.hh
+++ examples/shared/fsbl/boot_media_loader.hh
@@ -9,9 +9,10 @@
 #include <optional>
 #include <span>
 
-struct MemArea {
-	uint32_t start = 0;
-	uint32_t end = 0;
+struct AppImageInfo {
+	uint32_t load_addr = 0;
+	uint32_t entry_point = 0;
+	uint32_t size = 0;
 };
 
 class BootMediaLoader {
@@ -23,9 +24,10 @@ public:
 		printf_("Booted from %s (%x)\n", BootDetect::bootmethod_string(boot_method).data(), boot_method);
 	}
 
-	MemArea read_app_image_header()
+	AppImageInfo read_app_image_header()
 	{
 		BootImageDef::image_header header;
+		static_assert(sizeof(header) == BootImageDef::HeaderSize);
 
 		switch (_bootmethod) {
 			case BootDetect::BOOT_NOR:
@@ -40,86 +42,99 @@ public:
 				panic("Unknown boot method");
 				break;
 		}
-		return _parse_header(header);
+
+		_parse_header(header);
+
+		return _image_info;
 	}
 
 	bool load_image()
 	{
-		if (!header_parsed)
+		if (!_header_parsed)
 			read_app_image_header();
 
+		bool ok;
 		switch (_bootmethod) {
-			case BootDetect::BOOT_NOR: {
-				auto ok = BootNorLoader::load_image(_spl_image.load_addr, _spl_image.size);
-				if (!ok)
-					panic("Failed reading NOR Flash\n");
-				return ok;
+			case BootDetect::BOOT_NOR:
+				ok = BootNorLoader::load_image(_image_info.load_addr, _image_info.size);
 				break;
-			}
 
-			case BootDetect::BOOT_SDCARD: {
-				auto ok = BootSDLoader::load_image(_spl_image.load_addr, _spl_image.size);
-				if (!ok)
-					panic("Failed reading SD Card\n");
-				return ok;
+			case BootDetect::BOOT_SDCARD:
+				ok = BootSDLoader::load_image(_image_info.load_addr, _image_info.size);
 				break;
-			}
 
 			default:
 				panic("Unknown boot method");
-				return false;
 				break;
 		}
+
+		if (!ok)
+			panic("Failed reading Boot media\n");
+
+		_image_loaded = true;
+		return ok;
 	}
 
 	typedef void __attribute__((noreturn)) (*image_entry_noargs_t)(void);
 	void boot_image()
 	{
-		if (!header_parsed)
+		if (!_header_parsed)
 			read_app_image_header();
 
-		uint32_t entry_addr = _spl_image.entry_point;
-		auto image_entry = reinterpret_cast<image_entry_noargs_t>(_spl_image.entry_point);
-		debug("image entry point: 0x%lx\n", _spl_image->entry_point);
+		if (!_image_loaded)
+			load_image();
+
+		auto image_entry = reinterpret_cast<image_entry_noargs_t>(_image_info.entry_point);
+		debug("image entry point: 0x%lx\n", _image_info.entry_point);
 		image_entry();
 	}
 
 private:
 	BootDetect::BootMethod _bootmethod;
-	BootImageDef::spl_image_info _spl_image;
-	bool header_parsed = false;
+	bool _header_parsed = false;
+	bool _image_loaded = false;
+	AppImageInfo _image_info;
 
-	MemArea _parse_header(BootImageDef::image_header &header)
+	void _parse_header(BootImageDef::image_header &header)
 	{
 		uint32_t magic = be32_to_cpu(header.ih_magic);
 		if (magic == BootImageDef::IH_MAGIC) {
-			uint32_t header_size = sizeof(BootImageDef::image_header);
-
-			if (_spl_image.flags & BootImageDef::SPL_COPY_PAYLOAD_ONLY) {
+			if (header.ih_load == 0) {
+				printf_("ih_load is 0\n");
 				// On some system (e.g. powerpc), the load-address and
 				// entry-point is located at address 0. We can't load
 				// to 0-0x40. So skip header in this case.
-				_spl_image.load_addr = be32_to_cpu(header.ih_load);
-				_spl_image.entry_point = be32_to_cpu(header.ih_ep);
-				_spl_image.size = be32_to_cpu(header.ih_size);
+				_image_info.load_addr = be32_to_cpu(header.ih_load);
+				_image_info.entry_point = be32_to_cpu(header.ih_ep);
+				_image_info.size = be32_to_cpu(header.ih_size);
 			} else {
-				_spl_image.entry_point = be32_to_cpu(header.ih_load);
-				// Load including the header
-				_spl_image.load_addr = _spl_image.entry_point - header_size;
-				_spl_image.size = be32_to_cpu(header.ih_size) + header_size;
+				constexpr uint32_t header_size = sizeof(BootImageDef::image_header);
+				_image_info.entry_point = be32_to_cpu(header.ih_load);
+				_image_info.load_addr = _image_info.entry_point - header_size;
+				_image_info.size = be32_to_cpu(header.ih_size) + header_size;
 			}
+			debug("Raw header (big-endian):\n");
+			debug("  ih_magic: %x\n", header.ih_magic);
+			debug("  ih_hcrc: %x\n", header.ih_hcrc);
+			debug("  ih_time: %x\n", header.ih_time);
+			debug("  ih_size: %x\n", header.ih_size);
+			debug("  ih_load: %x\n", header.ih_load);
+			debug("  ih_ep: %x\n", header.ih_ep);
+			debug("  ih_dcrc: %x\n", header.ih_dcrc);
+			debug("  ih_os: %x\n", header.ih_os);
+			debug("  ih_arch: %x\n", header.ih_arch);
+			debug("  ih_type: %x\n", header.ih_type);
+			debug("  ih_comp: %x\n", header.ih_comp);
+			debug("  ih_name: %32s\n", header.ih_name);
 
-			_spl_image.os = be32_to_cpu(header.ih_os);
-			_spl_image.name = (char *)header.ih_name;
-			debug(
-				"payload image: %32s load addr: 0x%lx size: %d\n", spl_image.name, spl_image.load_addr, spl_image.size);
+			debug("Image load addr: 0x%08x entry_addr: %08x size: %08x\n",
+				  _image_info.load_addr,
+				  _image_info.entry_point,
+				  _image_info.size);
 
-			header_parsed = true;
-			return {_spl_image.load_addr, _spl_image.size};
-		};
-		// TODO: Handle raw images
-
-		panic("Failed to read a valid header: magic was %x, expected %x\n", magic, BootImageDef::IH_MAGIC);
-		return {0, 0};
+			_header_parsed = true;
+		} else
+			// TODO: Handle raw images
+			panic("Failed to read a valid header: magic was %x, expected %x\n", magic, BootImageDef::IH_MAGIC);
 	}
 };
