b507c1ba28d32ff1ff6ef7cd78759eb52585ded7
Dan Green <danngreen1@gmail.com>
2022-06-27 15:12:17 -0700

    Cleanup main

--- examples/shared/fsbl/main.cc
+++ examples/shared/fsbl/main.cc
@@ -1,30 +1,22 @@
-#include "stm32mp157cxx_ca7.h"
-
+#include "boot_media_loader.hh"
 #include "clocks.hh"
+#include "ddr/ram_tests.hh"
+#include "ddr/stm32mp1_ram.h"
 #include "delay.h"
 #include "drivers/leds.hh"
 #include "drivers/uart.hh"
 #include "mmp9_conf.hh"
-
-#include "ddr/ram_tests.hh"
-#include "ddr/stm32mp1_ram.h"
-
-#include "boot_media_loader.hh"
-#include "boot_nor.hh"
-
 #include "printf/printf.h"
+#include "stm32mp157cxx_ca7.h"
 
 namespace Board = MMp9;
-
-void delay(unsigned cycles);
 
 void main()
 {
 	SystemClocks::init_pll1_pll2();
 
 	Uart<Board::ConsoleUART> uart(Board::UartRX, Board::UartTX, 115200);
-
-	printf_("Initializing RAM\n");
+	printf_("\n\nInitializing RAM\n");
 	stm32mp1_ddr_setup();
 
 	printf_("Testing RAM\n");
@@ -33,16 +25,11 @@ void main()
 	const uint32_t ram_end = ram_start + ram_size;
 	RamTests::run_all(ram_start, ram_size);
 
-	BootMediaLoader loader{BootDetect::read_boot_method()};
+	auto boot_method = BootDetect::read_boot_method();
+	BootMediaLoader loader{boot_method};
 
 	printf_("Reading app image header\n");
-	auto app_memory_area = loader.read_app_image_header();
-	if (app_memory_area.start < ram_start || app_memory_area.end > ram_end)
-		panic("App will not fit in RAM: app %x to %x exceeds RAM %x to %x\n",
-			  app_memory_area.start,
-			  app_memory_area.end,
-			  ram_start,
-			  ram_end);
+	loader.read_app_image_header();
 
 	printf_("Loading app image\n");
 	loader.load_image();
@@ -53,7 +40,7 @@ void main()
 	// Should not reach here, but in case we do, blink LED rapidly
 	printf_("FAILED!\n");
 	Board::BlueLED blue_led;
-	constexpr uint32_t dlytime = 100000;
+	constexpr uint32_t dlytime = 50000;
 	while (1) {
 		blue_led.on();
 		udelay(dlytime);
