fd518c561e00edf89814bf4c96a4f2974215a481
Dan Green <danngreen1@gmail.com>
2022-06-28 17:13:22 -0700

    Init security (TZ, etc)

--- examples/shared/fsbl/main.cc
+++ examples/shared/fsbl/main.cc
@@ -10,16 +10,22 @@
 #include "stm32mp157cxx_ca7.h"
 
 namespace Board = MMp9;
+void security_init();
 
 void main()
 {
+	Board::BlueLED blue_led;
+	blue_led.off();
+	blue_led.on();
+
 	SystemClocks::init_pll1_pll2();
+	security_init();
 
 	Uart<Board::ConsoleUART> uart(Board::UartRX, Board::UartTX, 115200);
 	printf_("\n\nInitializing RAM\n");
 	stm32mp1_ddr_setup();
 
-	printf_("Testing RAM\n");
+	printf_("Testing RAM.\n");
 	const uint32_t ram_start = DRAM_MEM_BASE;
 	const auto ram_size = stm32mp1_ddr_get_size();
 	const uint32_t ram_end = ram_start + ram_size;
@@ -39,7 +45,6 @@ void main()
 
 	// Should not reach here, but in case we do, blink LED rapidly
 	printf_("FAILED!\n");
-	Board::BlueLED blue_led;
 	constexpr uint32_t dlytime = 50000;
 	while (1) {
 		blue_led.on();
@@ -53,4 +58,28 @@ void main()
 extern "C" void putchar_(char c)
 {
 	Uart<Board::ConsoleUART>::putchar(c);
+}
+
+void security_init()
+{
+	// Disable protection to RCC->BDCR, PWR->CR2, RTC, backup registers
+	PWR->CR1 = PWR->CR1 | PWR_CR1_DBP;
+	while (!(PWR->CR1 & PWR_CR1_DBP))
+		;
+
+	// Disable Trust Zone
+	RCC->TZCR = 0;
+
+	// Allow read/write for all securable peripherals (top 7 bits are reserved)
+	TZPC->DECPROT0 = 0x01FFFFFF;
+
+	// Allow non-secure access to SYSRAM
+	TZPC->TZMA1_SIZE = 0;
+
+	// Disable all tamper detection
+	TAMP->CR1 = 0;
+
+	// Allow non-secure access to GPIOZ
+	RCC->MP_AHB5ENSETR = RCC_MP_AHB5ENSETR_GPIOZEN;
+	GPIOZ->SECCFGR = 0;
 }
