469b930826a2b6068856fdfd8ab28addc112aac1
Dan Green <danngreen1@gmail.com>
2022-06-28 20:44:47 -0700

    QSPI prescaler set to 88MHz clock rate

--- examples/shared/fsbl/norflash/qspi_norflash_read.c
+++ examples/shared/fsbl/norflash/qspi_norflash_read.c
@@ -4,6 +4,13 @@
 
 #define QSPI_DUMMY_CYCLES_READ 0
 
+void QSPI_init()
+{
+	// ACLK is the default, 266MHz
+	QUADSPI->CR = QUADSPI->CR | QUADSPI_CR_PRESCALER_1; // 0b01 = prescale /3
+														// resulting clk = 88MHZ
+}
+
 uint32_t QSPI_read_SIO(uint8_t *pData, uint32_t read_addr, uint32_t num_bytes)
 {
 	LL_QSPI_WaitNotBusy();
--- examples/shared/fsbl/norflash/qspi_norflash_read.h
+++ examples/shared/fsbl/norflash/qspi_norflash_read.h
@@ -5,6 +5,7 @@
 extern "C" {
 #endif
 
+void QSPI_init();
 uint32_t QSPI_read_SIO(uint8_t *pData, uint32_t read_addr, uint32_t num_bytes);
 
 #ifdef __cplusplus
