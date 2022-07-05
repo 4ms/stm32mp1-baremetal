38923a034e2469cc9cae53d8a3f9d61df8c858cd
Dan Green <danngreen1@gmail.com>
2022-07-05 14:45:06 -0700

    Added more libc stubs so we can use std::optional

--- examples/shared/system/libc_stub.c
+++ examples/shared/system/libc_stub.c
@@ -1,4 +1,14 @@
 void _init(void) {}
 void _fini(void) {}
+void _exit(void)
+{
+	while (1)
+		;
+}
+void _kill(void) {}
+int _getpid(void)
+{
+	return 0;
+}
 int __errno;
 void *__dso_handle = (void *)&__dso_handle;
