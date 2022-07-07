d4edb29f15a02d86063f434a1ca21978953d0708
Dan Green <danngreen1@gmail.com>
2022-07-07 09:56:31 -0700

    Cleaned up uimg header script

--- scripts/uimg_header.py
+++ scripts/uimg_header.py
@@ -3,14 +3,27 @@ import sys
 import time
 import zlib
 
-magic = 0x27051956
-image_type_kernel = 2
-image_type_firmware = 5
-os_uboot = 17
+# U-Boot compatible "legacy" image header
+# Defined by U-Boot in u-boot repo: https://github.com/u-boot/u-boot/blob/master/include/image.h
+
+magic = 0x27051956       
+
+# These are the choices for some options in the U-Boot header
+# MP1-Boot ignores these, but if you're using this script with
+# U-Boot, then it might matter:
+
+os_linux = 5             # U-Boot uses this for Linux OS images
+os_uboot = 17            # U-Boot uses this for SSBL images
+
 arch_arm = 2
-compress_none = 0
-compress_gzip = 1
-compress_bzip2 = 2
+
+image_type_kernel = 2    # U-Boot uses this for Linux OS images
+image_type_firmware = 5  # U-Boot uses this for SSBL images
+
+compress_none = 0        # MP1-Boot only supports uncompressed kernels, for now
+# compress_gzip = 1
+# compress_bzip2 = 2
+
 
 with open(sys.argv[1], "rb") as bin_file:
     payload = bin_file.read();
@@ -18,30 +31,31 @@ with open(sys.argv[1], "rb") as bin_file:
 #TODO: these should be cmdline args:
 loadaddr = 0xC2000040
 entryaddr = 0xC2000040
-image_name = bytes("stm32mp1-baremetal image", "ascii")
+os = os_linux
+image_type = image_type_kernel
 compress = compress_none
-image_type = image_type_firmware
+image_name = bytes("stm32mp1-baremetal image", "ascii")
 
+# Calculate some header entries:
 datalen = len(payload)
 tmstamp = int(time.time())
-
 data_crc = zlib.crc32(payload) & 0xffffffff
 
-# To calc the header CRC, we generate a header with the CRC zero'ed out
-# Then calc the CRC of that, then fill it back in
-header_no_crc = struct.pack(">IIIIIIIbbbb32s", 
-    magic,                   # Image Header Magic Number	
-    0,
-    tmstamp,                 # Image Creation Timestamp	
-    datalen,                 # Image Data Size		
-    loadaddr,                # Data Load Address		
-    entryaddr,               # Entry Point Address		
-    data_crc,                # Image Data CRC Checksum	
-    os_uboot,                # Operating System		
-    arch_arm,                # CPU architecture		
-    image_type,              # Image Type			
-    compress,                # Compression Type		
-    image_name,              # Image Name		
+# To calc the header CRC, we generate a header with the CRC zero'ed out.
+# Then we calc the CRC of that, add fill the CRC value back in
+header_no_crc = struct.pack(">IIIIIIIbbbb32s",  # BigEndian: 4-byte ints * 7, bytes * 4, 32-byte string
+    magic,                                      # Image Header Magic Number	
+    0,                                          # Placeholder for header CRC
+    tmstamp,                                    # Image Creation Timestamp	
+    datalen,                                    # Image Data Size		
+    loadaddr,                                   # Data Load Address		
+    entryaddr,                                  # Entry Point Address		
+    data_crc,                                   # Image Data CRC Checksum	
+    os,                                         # Operating System		
+    arch_arm,                                   # CPU architecture		
+    image_type,                                 # Image Type			
+    compress,                                   # Compression Type		
+    image_name,                                 # Image Name		
     )
 
 hcrc = (zlib.crc32(header_no_crc) & 0xffffffff).to_bytes(4, 'big')
