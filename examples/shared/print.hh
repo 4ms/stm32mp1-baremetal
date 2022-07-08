65aa57832d8f5445fc501b548ba4e5b6b14da484
Dan Green <danngreen1@gmail.com>
2022-07-08 15:36:20 -0700

    Added Hex type for printing hexadecimal chars

--- examples/shared/print.hh
+++ examples/shared/print.hh
@@ -1,7 +1,13 @@
 #pragma once
 
+struct Hex {
+	unsigned x;
+};
+
+void printone(const unsigned char *str);
 void printone(const char *str);
 void printone(int value);
+void printone(Hex value);
 
 template<typename... Types>
 void print(Types... args)
