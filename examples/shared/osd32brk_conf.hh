93ef030fbe00619e9b9e9bf3c7f18638d99ca2ba
Dan Green <danngreen1@gmail.com>
2022-07-07 22:07:58 -0700

    Updated board conf files to new Led class

--- examples/shared/osd32brk_conf.hh
+++ examples/shared/osd32brk_conf.hh
@@ -4,11 +4,16 @@
 namespace OSD32BRK
 {
 
-using RedLED = Led<GPIOZ_BASE, 6, LedActive::Low>;
-using GreenLED = Led<GPIOZ_BASE, 7, LedActive::Low>;
-using RedLED2 = Led<GPIOI_BASE, 8, LedActive::Low>;
-using GreenLED2 = Led<GPIOI_BASE, 9, LedActive::Low>;
+using RedLED = Led<GPIO::Z, PinNum::_6, LedActive::Low>;
+using GreenLED = Led<GPIO::Z, PinNum::_7, LedActive::Low>;
+using RedLED2 = Led<GPIO::I, PinNum::_8, LedActive::Low>;
+using GreenLED2 = Led<GPIO::I, PinNum::_9, LedActive::Low>;
 
-using OrangeLED = RedLED2; //For compatibility with STM32MP1 Discovery board
-using BlueLED = GreenLED2; //For compatibility with STM32MP1 Discovery board
+using OrangeLED = GreenLED; // For compatibility with STM32MP1 Discovery board
+using BlueLED = RedLED2;	// For compatibility with STM32MP1 Discovery board
+
+constexpr uint32_t ConsoleUART = UART4_BASE;
+constexpr PinConf UartRX{GPIO::B, PinNum::_2, PinAF::AF_8};
+constexpr PinConf UartTX{GPIO::G, PinNum::_11, PinAF::AF_6};
+
 } // namespace OSD32BRK
