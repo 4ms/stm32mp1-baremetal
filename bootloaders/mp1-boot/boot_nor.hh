40c85d0ca50a5e9dbb791c98588a39a9e622012f
Dan Green <danngreen1@gmail.com>
2022-07-12 13:29:41 -0700

    trying SIO

--- bootloaders/mp1-boot/boot_nor.hh
+++ bootloaders/mp1-boot/boot_nor.hh
@@ -1,27 +1,28 @@
 #pragma once
 #include "boot_image_def.hh"
 #include "boot_loader.hh"
+#include "drivers/pinconf.hh"
 #include "norflash/qspi_norflash_read.h"
+#include "print_messages.hh"
 #include <cstdint>
 
 struct BootNorLoader : BootLoader {
 	BootNorLoader()
 	{
 		QSPI_init();
+		// PinConf d2{GPIO::F, PinNum::_7, PinAF::AF_9};
+		// PinConf d3{GPIO::A, PinNum::_1, PinAF::AF_9};
+		// d2.init(PinMode::Alt);
+		// d3.init(PinMode::Alt);
 	}
 
 	BootImageDef::image_header read_image_header() override
 	{
 		BootImageDef::image_header header;
 
-		// Debug breakpoint
-		volatile int i = 1;
-		while (1)
-			;
-
-		auto ok = QSPI_read_quad((uint8_t *)(&header), BootImageDef::NorFlashSSBLAddr, BootImageDef::HeaderSize);
+		auto ok = QSPI_read_SIO((uint8_t *)(&header), BootImageDef::NorFlashSSBLAddr, BootImageDef::HeaderSize);
 		if (!ok) {
-			// pr_err("Failed reading NOR Flash\n");
+			pr_err("Failed reading NOR Flash\n");
 			return {};
 		}
 
@@ -31,6 +32,6 @@ struct BootNorLoader : BootLoader {
 	bool load_image(uint32_t load_addr, uint32_t size) override
 	{
 		auto load_dst = reinterpret_cast<uint8_t *>(load_addr);
-		return QSPI_read_quad(load_dst, BootImageDef::NorFlashSSBLAddr, size);
+		return QSPI_read_SIO(load_dst, BootImageDef::NorFlashSSBLAddr, size);
 	}
 };
