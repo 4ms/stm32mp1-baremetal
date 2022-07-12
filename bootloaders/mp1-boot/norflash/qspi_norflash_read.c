b8b82b1ff78e3f71a6fb89ee9fa13c6ebb9a9f15
Dan Green <danngreen1@gmail.com>
2022-07-11 22:26:53 -0700

    WIP: quad read QSPI

--- bootloaders/mp1-boot/norflash/qspi_norflash_read.c
+++ bootloaders/mp1-boot/norflash/qspi_norflash_read.c
@@ -3,6 +3,7 @@
 #include "stm32mp1xx.h"
 
 #define QSPI_DUMMY_CYCLES_READ 0
+#define QSPI_DUMMY_CYCLES_QUAD_READ 8 // Datasheet says 4 bytes: 2 clks/byte * 4 = 8 clocks
 
 void QSPI_init()
 {
@@ -21,6 +22,21 @@ uint32_t QSPI_read_SIO(uint8_t *pData, uint32_t read_addr, uint32_t num_bytes)
 						  QSPI_INSTRUCTION_1_LINE | QSPI_DATA_1_LINE | (QSPI_DUMMY_CYCLES_READ << 18) |
 						  QSPI_ALTERNATE_BYTES_8_BITS | QSPI_ALTERNATE_BYTES_1_LINE | QSPI_ADDRESS_24_BITS |
 						  QSPI_ADDRESS_1_LINE | FAST_READ_CMD | QSPI_FUNCTIONAL_MODE_INDIRECT_READ);
+	LL_QPSI_SetAltBytes(0);
+	LL_QSPI_SetAddress(read_addr);
+
+	return LL_QSPI_Receive(pData);
+}
+
+uint32_t QSPI_read_quad(uint8_t *pData, uint32_t read_addr, uint32_t num_bytes)
+{
+	LL_QSPI_WaitNotBusy();
+
+	LL_QPSI_SetDataLength(num_bytes);
+	LL_QSPI_SetCommConfig(QSPI_DDR_MODE_DISABLE | QSPI_DDR_HHC_ANALOG_DELAY | QSPI_SIOO_INST_EVERY_CMD |
+						  QSPI_INSTRUCTION_1_LINE | QSPI_DATA_4_LINES | (QSPI_DUMMY_CYCLES_QUAD_READ << 18) |
+						  QSPI_ALTERNATE_BYTES_8_BITS | QSPI_ALTERNATE_BYTES_NONE | QSPI_ADDRESS_24_BITS |
+						  QSPI_ADDRESS_1_LINE | QUAD_OUT_FAST_READ_CMD | QSPI_FUNCTIONAL_MODE_INDIRECT_READ);
 	LL_QPSI_SetAltBytes(0);
 	LL_QSPI_SetAddress(read_addr);
 
