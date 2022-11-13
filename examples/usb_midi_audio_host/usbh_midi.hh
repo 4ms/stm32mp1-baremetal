6fd165c9bd92057acbdb2de358774742f967d594
Dan Green <danngreen1@gmail.com>
2022-11-12 16:21:33 -0800

    Refactoring CDC_HandleTypeDef and associated structs
    Added structs for Midi descriptors as per USB MIDI v1.0 specification (1999).
    Removed CDC-specific descriptors.
    Renamed other structs to describe usage with MidiStreaming

--- examples/usb_midi_audio_host/usbh_midi.hh
+++ examples/usb_midi_audio_host/usbh_midi.hh
@@ -23,88 +23,6 @@
 #include "usbh_core.h"
 #include "usbh_host.hH"
 
-/*Communication Class codes*/
-#define USB_CDC_CLASS 0x02U
-#define COMMUNICATION_INTERFACE_CLASS_CODE 0x02U
-
-/*Data Interface Class Codes*/
-#define DATA_INTERFACE_CLASS_CODE 0x0AU
-
-/*Communication sub class codes*/
-#define CDC_RESERVED 0x00U
-#define DIRECT_LINE_CONTROL_MODEL 0x01U
-#define ABSTRACT_CONTROL_MODEL 0x02U
-#define TELEPHONE_CONTROL_MODEL 0x03U
-#define MULTICHANNEL_CONTROL_MODEL 0x04U
-#define CAPI_CONTROL_MODEL 0x05U
-#define ETHERNET_NETWORKING_CONTROL_MODEL 0x06U
-#define ATM_NETWORKING_CONTROL_MODEL 0x07U
-
-/*Communication Interface Class Control Protocol Codes*/
-#define NO_CLASS_SPECIFIC_PROTOCOL_CODE 0x00U
-#define COMMON_AT_COMMAND 0x01U
-#define VENDOR_SPECIFIC 0xFFU
-
-#define CS_INTERFACE 0x24U
-#define CDC_PAGE_SIZE_64 0x40U
-
-/*Class-Specific Request Codes*/
-#define CDC_SEND_ENCAPSULATED_COMMAND 0x00U
-#define CDC_GET_ENCAPSULATED_RESPONSE 0x01U
-#define CDC_SET_COMM_FEATURE 0x02U
-#define CDC_GET_COMM_FEATURE 0x03U
-#define CDC_CLEAR_COMM_FEATURE 0x04U
-
-#define CDC_SET_AUX_LINE_STATE 0x10U
-#define CDC_SET_HOOK_STATE 0x11U
-#define CDC_PULSE_SETUP 0x12U
-#define CDC_SEND_PULSE 0x13U
-#define CDC_SET_PULSE_TIME 0x14U
-#define CDC_RING_AUX_JACK 0x15U
-
-#define CDC_SET_LINE_CODING 0x20U
-#define CDC_GET_LINE_CODING 0x21U
-#define CDC_SET_CONTROL_LINE_STATE 0x22U
-#define CDC_SEND_BREAK 0x23U
-
-#define CDC_SET_RINGER_PARMS 0x30U
-#define CDC_GET_RINGER_PARMS 0x31U
-#define CDC_SET_OPERATION_PARMS 0x32U
-#define CDC_GET_OPERATION_PARMS 0x33U
-#define CDC_SET_LINE_PARMS 0x34U
-#define CDC_GET_LINE_PARMS 0x35U
-#define CDC_DIAL_DIGITS 0x36U
-#define CDC_SET_UNIT_PARAMETER 0x37U
-#define CDC_GET_UNIT_PARAMETER 0x38U
-#define CDC_CLEAR_UNIT_PARAMETER 0x39U
-#define CDC_GET_PROFILE 0x3AU
-
-#define CDC_SET_ETHERNET_MULTICAST_FILTERS 0x40U
-#define CDC_SET_ETHERNET_POWER_MANAGEMENT_PATTERN FILTER 0x41U
-#define CDC_GET_ETHERNET_POWER_MANAGEMENT_PATTERN FILTER 0x42U
-#define CDC_SET_ETHERNET_PACKET_FILTER 0x43U
-#define CDC_GET_ETHERNET_STATISTIC 0x44U
-
-#define CDC_SET_ATM_DATA_FORMAT 0x50U
-#define CDC_GET_ATM_DEVICE_STATISTICS 0x51U
-#define CDC_SET_ATM_DEFAULT_VC 0x52U
-#define CDC_GET_ATM_VC_STATISTICS 0x53U
-
-/* wValue for SetControlLineState*/
-#define CDC_ACTIVATE_CARRIER_SIGNAL_RTS 0x0002U
-#define CDC_DEACTIVATE_CARRIER_SIGNAL_RTS 0x0000U
-#define CDC_ACTIVATE_SIGNAL_DTR 0x0001U
-#define CDC_DEACTIVATE_SIGNAL_DTR 0x0000U
-
-#define LINE_CODING_STRUCTURE_SIZE 0x07U
-/**
- * @}
- */
-
-/** @defgroup USBH_CDC_CORE_Exported_Types
- * @{
- */
-
 /* States for CDC State Machine */
 enum CDC_DataStateTypeDef {
 	CDC_IDLE = 0U,
@@ -116,173 +34,92 @@ enum CDC_DataStateTypeDef {
 
 enum CDC_StateTypeDef {
 	CDC_IDLE_STATE = 0U,
-	CDC_SET_LINE_CODING_STATE,
-	CDC_GET_LAST_LINE_CODING_STATE,
 	CDC_TRANSFER_DATA,
 	CDC_ERROR_STATE,
 };
 
-/* Header Functional Descriptor
---------------------------------------------------------------------------------
-Offset|  field              | Size  |    Value   |   Description
-------|---------------------|-------|------------|------------------------------
-0     |  bFunctionLength    | 1     |   number   |  Size of this descriptor.
-1     |  bDescriptorType    | 1     |   Constant |  CS_INTERFACE (0x24)
-2     |  bDescriptorSubtype | 1     |   Constant |  Identifier (ID) of functional
-	  |                     |       |            | descriptor.
-3     |  bcdCDC             | 2     |            |
-	  |                     |       |   Number   | USB Class Definitions for
-	  |                     |       |            | Communication Devices Specification
-	  |                     |       |            | release number in binary-coded
-	  |                     |       |            | decimal
-------|---------------------|-------|------------|------------------------------
-*/
-struct CDC_HeaderFuncDesc_TypeDef {
-	uint8_t bLength;			/*Size of this descriptor.*/
-	uint8_t bDescriptorType;	/*CS_INTERFACE (0x24)*/
-	uint8_t bDescriptorSubType; /* Header functional descriptor subtype as*/
-	uint16_t bcdCDC;			/* USB Class Definitions for Communication
-								  Devices Specification release number in
-								binary-coded decimal. */
-};
-/* Call Management Functional Descriptor
---------------------------------------------------------------------------------
-Offset|  field              | Size  |    Value   |   Description
-------|---------------------|-------|------------|------------------------------
-0     |  bFunctionLength    | 1     |   number   |  Size of this descriptor.
-1     |  bDescriptorType    | 1     |   Constant |  CS_INTERFACE (0x24)
-2     |  bDescriptorSubtype | 1     |   Constant |  Call Management functional
-	  |                     |       |            |  descriptor subtype.
-3     |  bmCapabilities     | 1     |   Bitmap   | The capabilities that this configuration
-	  |                     |       |            | supports:
-	  |                     |       |            | D7..D2: RESERVED (Reset to zero)
-	  |                     |       |            | D1: 0 - Device sends/receives call
-	  |                     |       |            | management information only over
-	  |                     |       |            | the Communication Class
-	  |                     |       |            | interface.
-	  |                     |       |            | 1 - Device can send/receive call
-	  |                     \       |            | management information over a
-	  |                     |       |            | Data Class interface.
-	  |                     |       |            | D0: 0 - Device does not handle call
-	  |                     |       |            | management itself.
-	  |                     |       |            | 1 - Device handles call
-	  |                     |       |            | management itself.
-	  |                     |       |            | The previous bits, in combination, identify
-	  |                     |       |            | which call management scenario is used. If bit
-	  |                     |       |            | D0 is reset to 0, then the value of bit D1 is
-	  |                     |       |            | ignored. In this case, bit D1 is reset to zero for
-	  |                     |       |            | future compatibility.
-4     | bDataInterface      | 1     | Number     | Interface number of Data Class interface
-	  |                     |       |            | optionally used for call management.
-------|---------------------|-------|------------|------------------------------
-*/
-struct CDC_CallMgmtFuncDesc_TypeDef {
-	uint8_t bLength;			/*Size of this functional descriptor, in bytes.*/
-	uint8_t bDescriptorType;	/*CS_INTERFACE (0x24)*/
-	uint8_t bDescriptorSubType; /* Call Management functional descriptor subtype*/
-	uint8_t bmCapabilities;		/* bmCapabilities: D0+D1 */
-	uint8_t bDataInterface;		/*bDataInterface: 1*/
-};
-/* Abstract Control Management Functional Descriptor
---------------------------------------------------------------------------------
-Offset|  field              | Size  |    Value   |   Description
-------|---------------------|-------|------------|------------------------------
-0     |  bFunctionLength    | 1     |   number   |  Size of functional descriptor, in bytes.
-1     |  bDescriptorType    | 1     |   Constant |  CS_INTERFACE (0x24)
-2     |  bDescriptorSubtype | 1     |   Constant |  Abstract Control Management
-	  |                     |       |            |  functional  descriptor subtype.
-3     |  bmCapabilities     | 1     |   Bitmap   | The capabilities that this configuration
-	  |                     |       |            | supports ((A bit value of zero means that the
-	  |                     |       |            | request is not supported.) )
-												   D7..D4: RESERVED (Reset to zero)
-	  |                     |       |            | D3: 1 - Device supports the notification
-	  |                     |       |            | Network_Connection.
-	  |                     |       |            | D2: 1 - Device supports the request
-	  |                     |       |            | Send_Break
-	  |                     |       |            | D1: 1 - Device supports the request
-	  |                     \       |            | combination of Set_Line_Coding,
-	  |                     |       |            | Set_Control_Line_State, Get_Line_Coding, and the
-												   notification Serial_State.
-	  |                     |       |            | D0: 1 - Device supports the request
-	  |                     |       |            | combination of Set_Comm_Feature,
-	  |                     |       |            | Clear_Comm_Feature, and Get_Comm_Feature.
-	  |                     |       |            | The previous bits, in combination, identify
-	  |                     |       |            | which requests/notifications are supported by
-	  |                     |       |            | a Communication Class interface with the
-	  |                     |       |            |   SubClass code of Abstract Control Model.
-------|---------------------|-------|------------|------------------------------
-*/
-struct CDC_AbstCntrlMgmtFuncDesc_TypeDef {
-	uint8_t bLength;			/*Size of this functional descriptor, in bytes.*/
-	uint8_t bDescriptorType;	/*CS_INTERFACE (0x24)*/
-	uint8_t bDescriptorSubType; /* Abstract Control Management functional
-								 descriptor subtype*/
-	uint8_t bmCapabilities;		/* The capabilities that this configuration supports */
-};
-/* Union Functional Descriptor
---------------------------------------------------------------------------------
-Offset|  field              | Size  |    Value   |   Description
-------|---------------------|-------|------------|------------------------------
-0     |  bFunctionLength    | 1     |   number   |  Size of this descriptor.
-1     |  bDescriptorType    | 1     |   Constant |  CS_INTERFACE (0x24)
-2     |  bDescriptorSubtype | 1     |   Constant |  Union functional
-	  |                     |       |            |  descriptor subtype.
-3     |  bMasterInterface   | 1     |   Constant | The interface number of the
-	  |                     |       |            | Communication or Data Class interface
-4     | bSlaveInterface0    | 1     | Number     | nterface number of first slave or associated
-	  |                     |       |            | interface in the union.
-------|---------------------|-------|------------|------------------------------
-*/
-struct CDC_UnionFuncDesc_TypeDef {
-	uint8_t bLength;			/*Size of this functional descriptor, in bytes*/
-	uint8_t bDescriptorType;	/*CS_INTERFACE (0x24)*/
-	uint8_t bDescriptorSubType; /* Union functional descriptor SubType*/
-	uint8_t bMasterInterface;	/* The interface number of the Communication or
-								Data Class interface,*/
-	uint8_t bSlaveInterface0;	/*Interface number of first slave*/
+struct MidiInterfaceHeaderDesc {
+	uint8_t bLength;			// Size of this descriptor = 7
+	uint8_t bDescriptorType;	// CS_INTERFACE (0x24)
+	uint8_t bDescriptorSubType; // MS_HEADER
+	uint16_t bcdMSC;			// Version = 0100
+	uint16_t wTotalLength;		// combined length of this desc header + all Jack and Element desc
 };
 
-struct CDC_InterfaceDesc_Typedef {
-	CDC_HeaderFuncDesc_TypeDef CDC_HeaderFuncDesc;
-	CDC_CallMgmtFuncDesc_TypeDef CDC_CallMgmtFuncDesc;
-	CDC_AbstCntrlMgmtFuncDesc_TypeDef CDC_AbstCntrlMgmtFuncDesc;
-	CDC_UnionFuncDesc_TypeDef CDC_UnionFuncDesc;
+struct MidiInJackDesc {
+	uint8_t bLength;			// Size of this descriptor = 6
+	uint8_t bDescriptorType;	// CS_INTERFACE (0x24)
+	uint8_t bDescriptorSubType; // MIDI_IN_JACK
+	uint8_t bJackType;			// EMBEDDED or EXTERNAL
+	uint8_t bJackID;			// Unique ID for the MIDI IN Jack
+	uint8_t iJack;				// index of string descriptor
 };
 
-/* Structure for CDC process */
-struct CDC_CommItfTypedef {
+struct MidiOutJackDesc {
+	constexpr static size_t MAX_SOURCES = 16;
+	uint8_t bLength;			 // Size of this descriptor = 6 + 2*p, where p is number of pins
+	uint8_t bDescriptorType;	 // CS_INTERFACE (0x24)
+	uint8_t bDescriptorSubType;	 // MIDI_OUT_JACK
+	uint8_t bJackType;			 // EMBEDDED or EXTERNAL
+	uint8_t bJackID;			 // Unique ID for the MIDI OUT Jack
+	uint8_t bNrInputPins;		 // Number of Input Pins of this MIDI OUT Jack: p
+	struct Source {				 //
+		uint8_t bSourceID;		 // ID of the Entity to which this Input Pin is connected.
+		uint8_t bSourcePin;		 // Output Pin number of the Entity to which this Input Pin is connected.
+	};							 //
+	Source aSource[MAX_SOURCES]; // Array of SourceID and SourcePin, with p entries
+	uint8_t iJack;				 // index of string descriptor
+};
+
+struct MidiElementDesc {
+	constexpr static size_t MAX_SOURCES = 16;
+	constexpr static size_t MAX_CAPS = 16;
+	uint8_t bLength;				 // Size of this descriptor = 10 + 2*p + n, where p is number of pins
+	uint8_t bDescriptorType;		 // CS_INTERFACE (0x24)
+	uint8_t bDescriptorSubType;		 // ELEMENT
+	uint8_t bElementID;				 // Unique ID for the Element
+	uint8_t bNrInputPins;			 // Number of Input Pins of this Element: p
+	struct Source {					 //
+		uint8_t bSourceID;			 // ID of the Entity to which this Input Pin is connected.
+		uint8_t bSourcePin;			 // Output Pin number of the Entity to which this Input Pin is connected.
+	};								 //
+	Source aSource[MAX_SOURCES];	 // Array of SourceID and SourcePin, with p entries
+	uint8_t bNrOutputPins;			 // Number of Output Pins of this Element: q
+	uint8_t bInTerminalLink;		 // The Terminal ID of the Input Terminal to which this Element is connected
+	uint8_t bOutTerminalLink;		 // The Terminal ID of the Output Terminal to which this Element is connected
+	uint8_t bElCapsSize;			 // Number of bytes in the bmElementCaps field. n
+	uint8_t bmElementCaps[MAX_CAPS]; // capabilities array, size n
+	uint8_t iJack;					 // index of string descriptor
+};
+
+struct MidiInterfaceDesc {
+	MidiInterfaceHeaderDesc header;
+	MidiInJackDesc injack;
+	MidiOutJackDesc outjack;
+	MidiElementDesc element;
+};
+
+struct AudioControlItf {
 	EndPoint ControlEP;
-	// uint8_t NotifPipe;
-	// uint8_t NotifEp;
 	uint8_t buff[8];
-	// uint16_t NotifEpSize;
 };
 
-struct CDC_DataItfTypedef {
+struct MidiItf {
 	EndPoint InEP;
 	EndPoint OutEP;
-	// uint8_t InPipe;
-	// uint8_t OutPipe;
-	// uint8_t OutEp;
-	// uint8_t InEp;
 	uint8_t buff[8];
-	// uint16_t OutEpSize;
-	// uint16_t InEpSize;
 };
 
-/* Structure for CDC process */
 struct CDC_HandleTypeDef {
-	CDC_CommItfTypedef ControlItf;
-	CDC_DataItfTypedef DataItf;
+	AudioControlItf ControlItf;
+	MidiItf DataItf;
 	uint8_t *pTxData;
 	uint8_t *pRxData;
 	uint32_t TxDataLength;
 	uint32_t RxDataLength;
-	CDC_InterfaceDesc_Typedef CDC_Desc;
 	CDC_StateTypeDef state;
 	CDC_DataStateTypeDef data_tx_state;
 	CDC_DataStateTypeDef data_rx_state;
-	uint8_t Rx_Poll;
 };
 
 extern USBH_ClassTypeDef MIDI_Class;
