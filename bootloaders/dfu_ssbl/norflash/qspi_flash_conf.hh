b47c896eb7fcc331d46cfd2b7ce385b7303a0d23
Dan Green <danngreen1@gmail.com>
2023-03-31 22:36:27 -0700

    QSPI Flash config uses Board:: namespace, not Brainboard::

--- bootloaders/dfu_ssbl/norflash/qspi_flash_conf.hh
+++ bootloaders/dfu_ssbl/norflash/qspi_flash_conf.hh
@@ -1,16 +1,12 @@
 #pragma once
-// FIXME: we need a more general way of setting the board conf
-// See mp1-boot repo
-#include "brainboard_p3_conf.hh"
+#include "board_conf.hh"
 #include "norflash/qspi_flash_struct.hh"
 
 constexpr mdrivlib::QSPIFlashConfig qspi_flash_conf = {
 	.io0 = {GPIO::F, PinNum::_8, PinAF::AF_10},
 	.io1 = {GPIO::F, PinNum::_9, PinAF::AF_10},
-
-	.io2 = Brainboard_p3::NORFlash::d2,
-	.io3 = Brainboard_p3::NORFlash::d3,
-
+	.io2 = Board::NORFlash::d2,
+	.io3 = Board::NORFlash::d3,
 	.clk = {GPIO::F, PinNum::_10, PinAF::AF_9},
 	.cs = {GPIO::B, PinNum::_6, PinAF::AF_10},
 
