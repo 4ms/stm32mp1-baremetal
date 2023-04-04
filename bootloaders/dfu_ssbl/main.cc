821fd869dae69b943626d56507cf9e57055cc9e1
Dan Green <danngreen1@gmail.com>
2023-04-03 21:49:05 -0700

    Always go into DFU mode (FSBL handles pin reading and branching)

--- bootloaders/dfu_ssbl/main.cc
+++ bootloaders/dfu_ssbl/main.cc
@@ -18,32 +18,22 @@
 namespace
 {
 Uart<Board::ConsoleUART> uart;
-bool dfu_mode_requested();
 } // namespace
 
 void main()
 {
-	// Check DFU pin:
-	// If it's open (high) -->  Try to Jump to App or else fall-through to DFU mode
-	// If it's jumpered shut (low) --> run DFU mode
+	// auto boot_method = BootDetect::read_boot_method();
+	// print("Booted from ", BootDetect::bootmethod_string(boot_method).data(), "\n");
+	// print("Loading app image...\n");
 
-	if (!dfu_mode_requested()) {
-		auto boot_method = BootDetect::read_boot_method();
-		print("Booted from ", BootDetect::bootmethod_string(boot_method).data(), "\n");
-		print("Loading app image...\n");
+	// constexpr uint32_t AppFlashSectorAddr = 0x100000;
+	// BootMediaLoader loader{boot_method};
+	// bool image_ok = loader.load_image(BootLoader::ImageType::Kernel);
 
-		constexpr uint32_t AppFlashSectorAddr = 0x100000;
-		BootMediaLoader loader{boot_method};
-		bool image_ok = loader.load_image(BootLoader::ImageType::Kernel);
-
-		if (image_ok) {
-			print("Jumping to app\n");
-			loader.boot_image();
-		}
-		print("FAILED\n");
-	} else {
-		print("DFU Mode pin detected active.\n");
-	}
+	// if (image_ok) {
+	// 	print("Jumping to app\n");
+	// 	loader.boot_image();
+	// }
 
 	print("\n\nUSB DFU Loader Starting...\n");
 
@@ -57,8 +47,8 @@ void main()
 
 	print("Connect a USB cable to the computer\n");
 	print("Run `dfu-util --list` in a terminal and you should see this device.\n");
-	print("Note: This DFU loader only works with NOR Flash. TODO: SDMMC\n");
-	print("You must reboot after loading. TODO: auto-jump to app after detach\n");
+	print("Note: This DFU loader only works with NOR Flash. TODO: SDMMC and DDR RAM\n");
+	print("You must reboot after loading. TODO: auto-jump to app after detaching\n");
 
 	// Blink green1 light at 1Hz
 	uint32_t last_tm = 0;
@@ -76,22 +66,5 @@ void main()
 		}
 	}
 }
-
-namespace
-{
-bool dfu_mode_requested()
-{
-	Board::DFUBootPin.init(PinMode::Input, PinPull::Up, PinPolarity::Inverted);
-	uint32_t pin_read = 10000;
-	uint32_t dfu_pin_active = 0;
-
-	while (pin_read--) {
-		if (Board::DFUBootPin.read())
-			dfu_pin_active++;
-	}
-
-	return (dfu_pin_active > 5000);
-}
-} // namespace
 
 void putchar_s(const char c) { uart.putchar(c); }
