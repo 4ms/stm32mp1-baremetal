87849f7307cfa5e7355d383503080d57342abfcd
Dan Green <danngreen1@gmail.com>
2023-04-05 12:09:28 -0700

    Faster QSPI clock (88MHz)

--- examples/usb_dfu_device/board_conf/qspi_flash_conf.hh
+++ examples/usb_dfu_device/board_conf/qspi_flash_conf.hh
@@ -11,14 +11,14 @@ constexpr mdrivlib::QSPIFlashConfig qspi_flash_conf = {
 	.cs = {GPIO::B, PinNum::_6, PinAF::AF_10},
 
 	/* QSPI freq = QUADSPICLK /(1 + ClockPrescaler) = 266 MHz/(2+1) = 88 Mhz */
-	.clock_division = 3,
-	.IRQ_pri = 2,
-	.IRQ_subpri = 2,
+	.clock_division = 2,
+	.IRQ_pri = 0,
+	.IRQ_subpri = 0,
 
 	.flash_size_bytes = 0x0100'0000,
 	.flash_size_address_bits = mdrivlib::QSPIFlashConfig::AddrBits24,
 
-	.chip_id = mdrivlib::QSPIFlashConfig::W25Q128JV, // was S25FL
+	.chip_id = mdrivlib::QSPIFlashConfig::W25Q128JV,
 
 	.io_mode = mdrivlib::QSPIFlashConfig::QuadSPI,
 
