bebbd02384745e0f498e3d1736da6a2839f3d7e3
Dan Green <danngreen1@gmail.com>
2023-04-25 17:19:45 -0700

    Larger partition size for SSBL

--- scripts/partition-sdcard.sh
+++ scripts/partition-sdcard.sh
@@ -28,8 +28,8 @@ sudo sgdisk -o $1
 
 echo ""
 echo "Partitioning..."
-echo "sudo sgdisk --resize-table=128 -a 1 -n 1:34:545 -c 1:fsbl1 -n 2:546:1057 -c 2:fsbl2 -n 3:1058:5153 -c 3:ssbl -N 4 -c 4:prog -p $1"
-sudo sgdisk --resize-table=128 -a 1 -n 1:34:545 -c 1:fsbl1 -n 2:546:1057 -c 2:fsbl2 -n 3:1058:5153 -c 3:ssbl -N 4 -c 4:prog -p $1
+echo "sudo sgdisk --resize-table=128 -a 1 -n 1:34:545 -c 1:fsbl1 -n 2:546:1057 -c 2:fsbl2 -n 3:1058:17441 -c 3:ssbl -N 4 -c 4:prog -p $1"
+sudo sgdisk --resize-table=128 -a 1 -n 1:34:545 -c 1:fsbl1 -n 2:546:1057 -c 2:fsbl2 -n 3:1058:17441 -c 3:ssbl -N 4 -c 4:prog -p $1
 
 echo ""
 echo "Formatting partition 4 as FAT32"
